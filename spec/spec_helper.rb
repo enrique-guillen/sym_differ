6c7f01126614f0f9b1c3e4a1bb06461176557978
Enrique Guillen <1494guillenenrique@gmail.com>
2024-03-27 12:25:41 +0900

    add frozen_string_literal config to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -15,5 +17,6 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.warnings = true
   config.order = :random
+
   Kernel.srand config.seed
 end
