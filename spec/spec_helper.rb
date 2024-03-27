355c187704740aabe002a89c04b27c6e181094b0
Enrique Guillen <1494guillenenrique@gmail.com>
2024-03-27 12:26:36 +0900

    Make default formatter for one-file runs a one liner spec_helper config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,9 +9,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
+  (config.default_formatter = "doc") if config.files_to_run.one?
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
   config.disable_monkey_patching!
