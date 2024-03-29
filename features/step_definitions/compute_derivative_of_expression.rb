7a2d4eab8c56426576184483712852f65c09e034
Enrique Guillen <1494guillenenrique@gmail.com>
2024-03-29 18:11:29 +0900

    New compute_derivative_of_expression step definition

---
+++ features/step_definitions/compute_derivative_of_expression.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+Before do
+  @params = { expression: nil, variable: nil }
+end
+
+After do
+  @params = nil
+  @payload = nil
+end
+
+Given("the expression to differentiate is {}") do |expression|
+  @params[:expression] = expression
+end
+
+Given("the variable of the expression to differentiate with is {}") do |variable|
+  @params[:variable] = variable
+end
+
+When("the expression is computed") do
+  @payload =
+    SymDiffer::DerivativeOfExpressionGetter
+    .new
+    .get(@params[:expression], @params[:variable])
+end
+
+Then("the derivative expression is {}") do |derivative_expression|
+  expect(@payload).to have_attributes(derivative_expression:)
+end
