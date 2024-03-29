7de84333c6639286b61e3fdb24c3e1b76945d1dd
Enrique Guillen <1494guillenenrique@gmail.com>
2024-03-29 16:48:06 +0900

    New SymDiffer::DerivativeOfExpressionGetter shell driven by cucumber tests

---
+++ lib/sym_differ/derivative_of_expression_getter.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module SymDiffer
+  # Implements the use case for a user getting the derivative of an expression.
+  class DerivativeOfExpressionGetter
+    def get(expression, variable)
+    end
+  end
+end
