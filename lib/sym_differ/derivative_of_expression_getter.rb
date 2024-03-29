3b1f0f8ba7eed3345372f72e6cda12dd7d09dd1f
Enrique Guillen <1494guillenenrique@gmail.com>
2024-03-30 08:24:41 +0900

    Handle happy path of DerivativeOfExpressionGetter

--- lib/sym_differ/derivative_of_expression_getter.rb
+++ lib/sym_differ/derivative_of_expression_getter.rb
@@ -5,8 +5,39 @@ module SymDiffer
   class DerivativeOfExpressionGetter
     DerivativeOfExpressionGetterResponse = Struct.new(:successful?, :derivative_expression)
 
-    def get(_expression, _variable)
-      DerivativeOfExpressionGetterResponse.new(true, "1")
+    def initialize(expression_text_parser, differentiation_visitor_builder, expression_reducer, expression_textifier)
+      @expression_text_parser = expression_text_parser
+      @differentiation_visitor_builder = differentiation_visitor_builder
+      @expression_reducer = expression_reducer
+      @expression_textifier = expression_textifier
+    end
+
+    def get(expression_text, variable)
+      DerivativeOfExpressionGetterResponse.new(
+        true,
+        parse_expression_text(expression_text)
+          .then { |expression| derive_expression(expression, variable) }
+          .then { |expression| reduce_expression(expression) }
+          .then { |expression| textify_expression(expression) }
+      )
+    end
+
+    private
+
+    def parse_expression_text(expression_text)
+      @expression_text_parser.parse(expression_text)
+    end
+
+    def derive_expression(expression, variable)
+      @differentiation_visitor_builder.build(variable).derive(expression)
+    end
+
+    def reduce_expression(expression)
+      @expression_reducer.reduce(expression)
+    end
+
+    def textify_expression(expression)
+      @expression_textifier.textify(expression)
     end
   end
 end
--- spec/sym_differ/derivative_of_expression_getter_spec.rb
+++ spec/sym_differ/derivative_of_expression_getter_spec.rb
@@ -4,7 +4,53 @@ require "spec_helper"
 require "sym_differ/derivative_of_expression_getter"
 
 RSpec.describe SymDiffer::DerivativeOfExpressionGetter do
-  it do
-    expect(described_class.new.get("x", "x")).to have_attributes(successful?: true, derivative_expression: "1")
+  describe "#get" do
+    subject(:get) do
+      described_class
+        .new(expression_text_parser, differentiation_visitor_builder, expression_reducer, expression_textifier)
+        .get("x", "x")
+    end
+
+    before do
+      allow(expression_text_parser)
+        .to receive(:parse)
+        .with("x")
+        .and_return(parsed_expression)
+
+      allow(differentiation_visitor_builder)
+        .to receive(:build)
+        .with("x")
+        .and_return(differentiation_visitor)
+
+      allow(differentiation_visitor)
+        .to receive(:derive)
+        .with(parsed_expression)
+        .and_return(derivative_expression)
+
+      allow(expression_reducer)
+        .to receive(:reduce)
+        .with(derivative_expression)
+        .and_return(reduced_derivative_expression)
+
+      allow(expression_textifier)
+        .to receive(:textify)
+        .with(reduced_derivative_expression)
+        .and_return(textified_reduced_derivative_expression)
+    end
+
+    let(:expression_text_parser) { double(:expression_text_parser) }
+    let(:differentiation_visitor_builder) { double(:differentiation_visitor_builder) }
+    let(:differentiation_visitor) { double(:differentiation_visitor) }
+    let(:expression_reducer) { double(:expression_reducer) }
+    let(:expression_textifier) { double(:expression_textifier) }
+
+    let(:parsed_expression) { double(:parsed_expression) }
+    let(:derivative_expression) { double(:derivative_expression) }
+    let(:reduced_derivative_expression) { double(:reduced_derivative_expression) }
+    let(:textified_reduced_derivative_expression) { double(:textified_reduced_derivative_expression) }
+
+    it "returns the reduced_derivative_expression" do
+      expect(get).to have_attributes(successful?: true, derivative_expression: textified_reduced_derivative_expression)
+    end
   end
 end
