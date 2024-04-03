b7e687158abb29908b77a45770350d221ec9ad2d
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-03 13:03:13 +0900

    Require parent class in free_form_expression_text_language error classeS

--- lib/sym_differ/free_form_expression_text_language/empty_expression_text_error.rb
+++ lib/sym_differ/free_form_expression_text_language/empty_expression_text_error.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "sym_differ/unparseable_expression_text_error"
+
 module SymDiffer
   module FreeFormExpressionTextLanguage
     # Error to raise when an attempt to parse an empty string is made.
--- lib/sym_differ/free_form_expression_text_language/invalid_syntax_error.rb
+++ lib/sym_differ/free_form_expression_text_language/invalid_syntax_error.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "sym_differ/unparseable_expression_text_error"
+
 module SymDiffer
   module FreeFormExpressionTextLanguage
     class InvalidSyntaxError < UnparseableExpressionTextError
