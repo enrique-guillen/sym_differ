e0125ef1956552a333a5a5c3b3bcf8b3e4ab4508
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-05 12:19:29 +0900

    Incomplete implementation of reduction of chains of sum expressions

--- lib/sym_differ/expression_reducer.rb
+++ lib/sym_differ/expression_reducer.rb
@@ -4,7 +4,79 @@ module SymDiffer
   # Reduces the terms in the provided expression.
   class ExpressionReducer
     def reduce(expression)
-      expression
+      if expression.is_a?(SumExpression)
+        reduce_sum_expression(expression)
+      else
+        expression
+      end
+    end
+
+    private
+
+    def reduce_sum_expression(expression)
+      reduced_expression_a = reduce(expression.expression_a)
+      reduced_expression_b = reduce(expression.expression_b)
+
+      result = convert_if_sum_of_two_constant_expressions(reduced_expression_a, reduced_expression_b)
+
+      convert_if_sum_of_constant_and_nested_constant(result)
+    end
+
+    def convert_if_sum_of_two_constant_expressions(reduced_expression_a, reduced_expression_b)
+      if reduced_expression_a.is_a?(ConstantExpression) && reduced_expression_b.is_a?(ConstantExpression)
+        build_constant_expression(reduced_expression_a.value + reduced_expression_b.value)
+      else
+        build_sum_expression(reduced_expression_a, reduced_expression_b)
+      end
+    end
+
+    def convert_if_sum_of_constant_and_nested_constant(expression)
+      return expression unless expression.is_a?(SumExpression)
+
+      sub_expressions =
+        if expression.expression_b.is_a?(SumExpression) && expression.expression_a.is_a?(ConstantExpression)
+          combine_constants_of_expressions(expression.expression_a, expression.expression_b)
+        elsif expression.expression_a.is_a?(SumExpression) && expression.expression_b.is_a?(ConstantExpression)
+          combine_constants_of_expressions(expression.expression_b, expression.expression_a)&.reverse
+        end
+
+      return expression if sub_expressions.nil?
+
+      build_sum_expression(sub_expressions[0], sub_expressions[1])
+    end
+
+    def combine_constants_of_expressions(constant_expression, sum_expression)
+      value_and_sub_expression = extract_value_and_subexpression_from_sum_expression(sum_expression)
+      return if value_and_sub_expression.nil?
+
+      value, sub_expression = value_and_sub_expression
+
+      [build_constant_expression(constant_expression.value + value), sub_expression]
+    end
+
+    def extract_value_and_subexpression_from_sum_expression(expression)
+      # if expression.expression_b.is_a?(SumExpression)
+      # retrieve_nested_value_and_build_modified_expression(expression.expression_b)
+      # elsif expression.expression_a.is_a?(SumExpression)
+      # retrieve_nested_value_and_build_modified_expression(expression.expression_a)
+
+      if expression.expression_b.is_a?(ConstantExpression)
+        [expression.expression_b.value, expression.expression_a]
+      elsif expression.expression_a.is_a?(ConstantExpression)
+        [expression.expression_a.value, expression.expression_b]
+      end
+    end
+
+    def build_constant_expression(value)
+      SymDiffer::ConstantExpression.new(value)
+    end
+
+    def build_sum_expression(expression_a, expression_b)
+      SymDiffer::SumExpression.new(expression_a, expression_b)
+    end
+
+    def build_variable_expression(name)
+      SymDiffer::VariableExpression.new(name)
     end
   end
 end
--- spec/sym_differ/expression_reducer_spec.rb
+++ spec/sym_differ/expression_reducer_spec.rb
@@ -3,6 +3,8 @@
 require "spec_helper"
 require "sym_differ/expression_reducer"
 require "sym_differ/constant_expression"
+require "sym_differ/sum_expression"
+require "sym_differ/variable_expression"
 
 RSpec.describe SymDiffer::ExpressionReducer do
   describe "#reduce" do
@@ -12,6 +14,218 @@ RSpec.describe SymDiffer::ExpressionReducer do
       let(:expression) { SymDiffer::ConstantExpression.new(1) }
 
       it { is_expected.to have_attributes(value: 1) }
+    end
+
+    context "when the expression is <SumExpression:<ConstantExpression:1>,<ConstantExpression:1>>" do
+      let(:expression) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::ConstantExpression.new(1)
+        )
+      end
+
+      it { is_expected.to have_attributes(value: 2) }
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<SumExpression:<<ConstantExpression:1>,<ConstantExpression:1>>," \
+            "<SumExpression:<<ConstantExpression:1>,<ConstantExpression:1>>" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::ConstantExpression.new(1)
+        )
+      end
+
+      let(:expression_b) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::ConstantExpression.new(1)
+        )
+      end
+
+      it { is_expected.to have_attributes(value: 4) }
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<SumExpression:<<ConstantExpression:1>,<ConstantExpression:1>>," \
+            "<VariableExpression:x>>" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::ConstantExpression.new(1)
+        )
+      end
+
+      let(:expression_b) { SymDiffer::VariableExpression.new("x") }
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(value: 2),
+          expression_b: an_object_having_attributes(name: "x")
+        )
+      end
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<ConstantExpression:1>," \
+            "<SumExpression:<VariableExpression:x>,<ConstantExpression:1>>>" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::ConstantExpression.new(1)
+      end
+
+      let(:expression_b) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::VariableExpression.new("x"),
+          SymDiffer::ConstantExpression.new(1)
+        )
+      end
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(value: 2),
+          expression_b: an_object_having_attributes(name: "x")
+        )
+      end
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<SumExpression:<VariableExpression:x>,<ConstantExpression:1>>," \
+            "<ConstantExpression:1>>" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::VariableExpression.new("x"),
+          SymDiffer::ConstantExpression.new(1)
+        )
+      end
+
+      let(:expression_b) do
+        SymDiffer::ConstantExpression.new(1)
+      end
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(name: "x"),
+          expression_b: an_object_having_attributes(value: 2)
+        )
+      end
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<ConstantExpression:1>," \
+            "<SumExpression:<ConstantExpression:1>,<VariableExpression:x>>>" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::ConstantExpression.new(1)
+      end
+
+      let(:expression_b) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::VariableExpression.new("x")
+        )
+      end
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(value: 2),
+          expression_b: an_object_having_attributes(name: "x")
+        )
+      end
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<SumExpression:<ConstantExpression:1>,<VariableExpression:x>>," \
+            "<ConstantExpression:1>>" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::VariableExpression.new("x")
+        )
+      end
+
+      let(:expression_b) do
+        SymDiffer::ConstantExpression.new(1)
+      end
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(name: "x"),
+          expression_b: an_object_having_attributes(value: 2)
+        )
+      end
+    end
+
+    context "when the expression is <SumExpression:" \
+            "<ConstantExpression:1>" \
+            "<SumExpression:<ConstantExpression:1>,<SumExpression:<VariableExpression:x>,<ConstantExpression:2>>>," \
+            ">" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::ConstantExpression.new(1)
+      end
+
+      let(:expression_b) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::SumExpression.new(
+            SymDiffer::VariableExpression.new("x"),
+            SymDiffer::ConstantExpression.new(2)
+          )
+        )
+      end
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(value: 4),
+          expression_b: an_object_having_attributes(name: "x")
+        )
+      end
+    end
+
+    context "when the expression is <SumExpression:" \
+            "  <ConstantExpression:1>" \
+            "  <SumExpression:" \
+            "    <ConstantExpression:1>," \
+            "    <SumExpression:" \
+            "      <VariableExpression:x>," \
+            "      <SumExpression:<VariableExpression:x>,<ConstantExpression:1>>>>," \
+            ">" do
+      let(:expression) { SymDiffer::SumExpression.new(expression_a, expression_b) }
+
+      let(:expression_a) do
+        SymDiffer::ConstantExpression.new(1)
+      end
+
+      let(:expression_b) do
+        SymDiffer::SumExpression.new(
+          SymDiffer::ConstantExpression.new(1),
+          SymDiffer::SumExpression.new(
+            SymDiffer::VariableExpression.new("x"),
+            SymDiffer::SumExpression.new(SymDiffer::VariableExpression.new("x"), SymDiffer::ConstantExpression.new(1))
+          )
+        )
+      end
+
+
+      it "returns the expected reduction" do
+        expect(reduce).to have_attributes(
+          expression_a: an_object_having_attributes(value: 3),
+          expression_b: an_object_having_attributes(name: "x")
+        )
+      end
     end
   end
 end
