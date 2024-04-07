9a2740b288924de48769debece9987692e4182e2
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-07 15:34:51 +0900

    Add missing requires to get_derivative_of_expression

--- features/step_definitions/compute_derivative_of_expression.rb
+++ features/step_definitions/compute_derivative_of_expression.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "sym_differ/derivative_of_expression_getter"
+require "sym_differ/free_form_expression_text_language/parser"
+require "sym_differ/differentiation/differentiation_visitor"
+require "sym_differ/expression_reducer"
+require "sym_differ/inline_printing/printing_visitor"
+
 Before do
   @params = { expression: nil, variable: nil }
 end
@@ -21,7 +27,10 @@ end
 When("the expression is computed") do
   @payload =
     SymDiffer::DerivativeOfExpressionGetter
-    .new
+    .new(SymDiffer::FreeFormExpressionTextLanguage::Parser.new,
+         DifferentiationVisitorBuilder.new,
+         SymDiffer::ExpressionReducer.new,
+         SymDiffer::InlinePrinting::PrintingVisitor.new)
     .get(@params[:expression], @params[:variable])
 end
 
