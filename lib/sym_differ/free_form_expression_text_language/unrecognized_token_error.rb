050ec02382bbcffe23a112bad8c8b0471ca2081a
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-10 15:24:43 +0900

    Document UnrecognizedTokenError

--- lib/sym_differ/free_form_expression_text_language/unrecognized_token_error.rb
+++ lib/sym_differ/free_form_expression_text_language/unrecognized_token_error.rb
@@ -4,6 +4,7 @@ require "sym_differ/unparseable_expression_text_error"
 
 module SymDiffer
   module FreeFormExpressionTextLanguage
+    # Raised when an expression is not parseable because the text contained unrecognized characters.
     class UnrecognizedTokenError < StandardError
       def initialize(invalid_expression_text)
         super()
