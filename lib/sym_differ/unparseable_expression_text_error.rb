906c173d584c6b9d8152ffd5327477f033e90ac3
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-10 14:54:12 +0900

    UnparseableExpressionTextError no longer takes arguments

--- lib/sym_differ/unparseable_expression_text_error.rb
+++ lib/sym_differ/unparseable_expression_text_error.rb
@@ -3,11 +3,5 @@
 module SymDiffer
   # The expression parser cannot parse an expression because the provided variable is considered invalid.
   class UnparseableExpressionTextError < StandardError
-    def initialize(invalid_expression_text)
-      super()
-      @invalid_expression_text = invalid_expression_text
-    end
-
-    attr_reader :invalid_expression_text
   end
 end
