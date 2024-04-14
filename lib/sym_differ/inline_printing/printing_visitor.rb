2fbf5a8b01ca4f839297d15fb3de875862d03055
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-14 17:49:36 +0900

    New PrintingVisitor#visit_subtract_expression

--- lib/sym_differ/inline_printing/printing_visitor.rb
+++ lib/sym_differ/inline_printing/printing_visitor.rb
@@ -4,6 +4,12 @@ module SymDiffer
   module InlinePrinting
     # Methods for displaying all the elements of an expression into a single inline string.
     class PrintingVisitor
+      def initialize(parenthesize_subtraction_expressions: false)
+        @parenthesize_subtraction_expressions = parenthesize_subtraction_expressions
+      end
+
+      attr_reader :parenthesize_subtraction_expressions
+
       def visit_constant_expression(expression)
         expression.value.to_s
       end
@@ -13,11 +19,52 @@ module SymDiffer
       end
 
       def visit_negate_expression(expression)
-        "-#{expression.negated_expression.accept(self)}"
+        stringified_negated_expression = stringify_expression(expression.negated_expression)
+
+        prefix_with_dash(stringified_negated_expression)
       end
 
       def visit_sum_expression(expression)
-        "#{expression.expression_a.accept(self)} + #{expression.expression_b.accept(self)}"
+        stringified_expression_a = stringify_expression(expression.expression_a)
+        stringified_expression_b = stringify_expression(expression.expression_b)
+
+        join_with_plus_sign(stringified_expression_a, stringified_expression_b)
+      end
+
+      def visit_subtract_expression(expression)
+        nested_visitor = build_visitor(parenthesize_subtraction_expressions: true)
+
+        stringified_minuend = stringify_expression(expression.minuend)
+        stringified_subtrahend = stringify_expression(expression.subtrahend, visitor: nested_visitor)
+        result = join_with_dash(stringified_minuend, stringified_subtrahend)
+
+        @parenthesize_subtraction_expressions ? surround_in_parenthesis(result) : result
+      end
+
+      private
+
+      def prefix_with_dash(expression)
+        "-#{expression}"
+      end
+
+      def join_with_plus_sign(expression_a, expression_b)
+        "#{expression_a} + #{expression_b}"
+      end
+
+      def join_with_dash(expression_a, expression_b)
+        "#{expression_a} - #{expression_b}"
+      end
+
+      def surround_in_parenthesis(expression)
+        "(#{expression})"
+      end
+
+      def stringify_expression(expression, visitor: self)
+        expression.accept(visitor)
+      end
+
+      def build_visitor(parenthesize_subtraction_expressions: true)
+        self.class.new(parenthesize_subtraction_expressions:)
       end
     end
   end
--- spec/sym_differ/inline_printing/printing_visitor_spec.rb
+++ spec/sym_differ/inline_printing/printing_visitor_spec.rb
@@ -5,11 +5,11 @@ require "sym_differ/inline_printing/printing_visitor"
 require "sym_differ/constant_expression"
 
 RSpec.describe SymDiffer::InlinePrinting::PrintingVisitor do
-  let(:visitor) { described_class.new }
+  let(:printing_visitor) { described_class.new }
 
   describe "#visit_constant_expression" do
     subject(:visit_constant_expression) do
-      visitor.visit_constant_expression(SymDiffer::ConstantExpression.new(1))
+      printing_visitor.visit_constant_expression(SymDiffer::ConstantExpression.new(1))
     end
 
     it { is_expected.to eq("1") }
@@ -17,7 +17,7 @@ RSpec.describe SymDiffer::InlinePrinting::PrintingVisitor do
 
   describe "#visit_variable_expression" do
     subject(:visit_variable_expression) do
-      visitor.visit_variable_expression(SymDiffer::VariableExpression.new("var"))
+      printing_visitor.visit_variable_expression(SymDiffer::VariableExpression.new("var"))
     end
 
     it { is_expected.to eq("var") }
@@ -25,13 +25,13 @@ RSpec.describe SymDiffer::InlinePrinting::PrintingVisitor do
 
   describe "#visit_negate_expression" do
     subject(:visit_negate_expression) do
-      visitor.visit_negate_expression(expression)
+      printing_visitor.visit_negate_expression(expression)
     end
 
     before do
       allow(negated_expression)
         .to receive(:accept)
-        .with(visitor)
+        .with(printing_visitor)
         .and_return("subexp")
     end
 
@@ -43,18 +43,18 @@ RSpec.describe SymDiffer::InlinePrinting::PrintingVisitor do
 
   describe "#visit_sum_expression" do
     subject(:visit_sum_expression) do
-      visitor.visit_sum_expression(expression)
+      printing_visitor.visit_sum_expression(expression)
     end
 
     before do
       allow(expression_a)
         .to receive(:accept)
-        .with(visitor)
+        .with(printing_visitor)
         .and_return("exp_a")
 
       allow(expression_b)
         .to receive(:accept)
-        .with(visitor)
+        .with(printing_visitor)
         .and_return("exp_b")
     end
 
@@ -63,5 +63,91 @@ RSpec.describe SymDiffer::InlinePrinting::PrintingVisitor do
     let(:expression_b) { double(:expression_b) }
 
     it { is_expected.to eq("exp_a + exp_b") }
+  end
+
+  describe "#visit_subtract_expression" do
+    subject(:visit_subtract_expression) do
+      printing_visitor.visit_subtract_expression(expression)
+    end
+
+    context "when the subtrahend is an arbitrary expression" do
+      let(:expression) { SymDiffer::SubtractExpression.new(minuend, subtrahend) }
+      let(:minuend) { double(:minuend) }
+      let(:subtrahend) { double(:subtrahend) }
+
+      before do
+        allow(minuend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: false))
+          .and_return("exp_a")
+
+        allow(subtrahend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: true))
+          .and_return("exp_b")
+      end
+
+      it { is_expected.to eq("exp_a - exp_b") }
+    end
+
+    context "when the right hand expression (subtrahend) is another subtraction expression" do
+      let(:expression) { SymDiffer::SubtractExpression.new(minuend, subtrahend) }
+
+      let(:minuend) { double(:minuend) }
+      let(:subtrahend) { SymDiffer::SubtractExpression.new(right_minuend, right_subtrahend) }
+
+      let(:right_minuend) { double(:right_minuend) }
+      let(:right_subtrahend) { double(:right_subtrahend) }
+
+      before do
+        allow(minuend).to receive(:accept).with(printing_visitor).and_return("minuend")
+
+        allow(right_minuend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: true))
+          .and_return("right_minuend")
+
+        allow(right_subtrahend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: true))
+          .and_return("right_subtrahend")
+      end
+
+      it { is_expected.to eq("minuend - (right_minuend - right_subtrahend)") }
+    end
+
+    context "when the expression is <SubtractExpression:<SubTractExpression:<x, <SubtractExpression:<x,x>>>,<x>>" do
+      let(:expression) { SymDiffer::SubtractExpression.new(minuend, subtrahend) }
+
+      let(:minuend) { SymDiffer::SubtractExpression.new(left_minuend, left_subtrahend) }
+      let(:subtrahend) { double(:subtrahend) }
+
+      let(:left_minuend) { double(:left_minuend) }
+      let(:left_subtrahend) { SymDiffer::SubtractExpression.new(left_right_minuend, left_right_subtrahend) }
+
+      let(:left_right_minuend) { double(:left_right_minuend) }
+      let(:left_right_subtrahend) { double(:left_right_subtrahend) }
+
+      before do
+        allow(subtrahend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: true))
+          .and_return("subtrahend")
+
+        allow(left_minuend).to receive(:accept).with(printing_visitor).and_return("left_minuend")
+
+        allow(left_right_minuend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: true))
+          .and_return("left_right_minuend")
+
+        allow(left_right_subtrahend)
+          .to receive(:accept)
+          .with(an_object_having_attributes(parenthesize_subtraction_expressions: true))
+          .and_return("left_right_subtrahend")
+      end
+
+      it { is_expected.to eq("left_minuend - (left_right_minuend - left_right_subtrahend) - subtrahend") }
+    end
   end
 end
