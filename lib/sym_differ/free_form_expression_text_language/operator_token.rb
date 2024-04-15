3e41eacfa1f3e34f8abfc121ad91aaafef736c1b
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-15 12:00:23 +0900

    In OperatorToken, provide unique entry points to create expressions

--- lib/sym_differ/free_form_expression_text_language/operator_token.rb
+++ lib/sym_differ/free_form_expression_text_language/operator_token.rb
@@ -15,15 +15,35 @@ module SymDiffer
       attr_reader :symbol
 
       def transform_into_expression(expression_a, expression_b)
-        if @symbol == "+"
-          SumExpression.new(expression_a, expression_b)
-        elsif @symbol == "-"
-          if expression_a.nil?
-            NegateExpression.new(expression_b)
-          else
-            SubtractExpression.new(expression_a, expression_b)
-          end
+        return build_sum_expression(expression_a, expression_b) if symbol_is_plus_sign?
+
+        if symbol_is_dash? && expression_a.nil?
+          build_negate_expression(expression_b)
+        elsif symbol_is_dash?
+          build_subtract_expression(expression_a, expression_b)
         end
+      end
+
+      private
+
+      def symbol_is_plus_sign?
+        @symbol == "+"
+      end
+
+      def symbol_is_dash?
+        @symbol == "-"
+      end
+
+      def build_sum_expression(expression_a, expression_b)
+        SumExpression.new(expression_a, expression_b)
+      end
+
+      def build_negate_expression(expression)
+        NegateExpression.new(expression)
+      end
+
+      def build_subtract_expression(expression_a, expression_b)
+        SubtractExpression.new(expression_a, expression_b)
       end
     end
   end
