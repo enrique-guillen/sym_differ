5ef849aa9ef9febc6de6b9d4bc8e6fbcde871663
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-19 19:27:01 +0900

    BuildSubtractExpressionCommand supports building subtract expression commands

--- lib/sym_differ/free_form_expression_text_language/build_subtract_expression_command.rb
+++ lib/sym_differ/free_form_expression_text_language/build_subtract_expression_command.rb
@@ -7,14 +7,23 @@ module SymDiffer
     # Builds a SubtractExpression out of the provided arguments.
     class BuildSubtractExpressionCommand
       def execute(arguments)
-        minuend, subtrahend = arguments
-        build_subtract_expression(minuend, subtrahend)
+        if arguments.size == 1
+          negated_expression = arguments.first
+          build_negate_expression(negated_expression)
+        else
+          minuend, subtrahend = arguments
+          build_subtract_expression(minuend, subtrahend)
+        end
       end
 
       private
 
       def build_subtract_expression(minuend, subtrahend)
         SubtractExpression.new(minuend, subtrahend)
+      end
+
+      def build_negate_expression(negated_expression)
+        NegateExpression.new(negated_expression)
       end
     end
   end
--- spec/sym_differ/free_form_expression_text_language/build_subtract_expression_command_spec.rb
+++ spec/sym_differ/free_form_expression_text_language/build_subtract_expression_command_spec.rb
@@ -6,11 +6,23 @@ require "sym_differ/subtract_expression"
 
 RSpec.describe SymDiffer::FreeFormExpressionTextLanguage::BuildSubtractExpressionCommand do
   describe "#execute" do
-    subject(:execute) { described_class.new.execute([minuend, subtrahend]) }
+    subject(:execute) { described_class.new.execute(arguments) }
 
-    let(:minuend) { double(:minuend) }
-    let(:subtrahend) { double(:subtrahend) }
+    context "when the arguments have two expressions" do
+      let(:arguments) { [minuend, subtrahend] }
 
-    it { is_expected.to have_attributes(minuend:, subtrahend:) }
+      let(:minuend) { double(:minuend) }
+      let(:subtrahend) { double(:subtrahend) }
+
+      it { is_expected.to have_attributes(minuend:, subtrahend:) }
+    end
+
+    context "when the arguments have only one expression" do
+      let(:arguments) { [negated_expression] }
+
+      let(:negated_expression) { double(:negated_expression) }
+
+      it { is_expected.to have_attributes(negated_expression:) }
+    end
   end
 end
