e641e909db4f8ae8cc719bb79bb05fc4570a9933
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-21 15:44:34 +0900

    BuildSumExpressionCommand supports creating PositiveExpressions if given one argument only

--- lib/sym_differ/expression_text_language_compiler/build_sum_expression_command.rb
+++ lib/sym_differ/expression_text_language_compiler/build_sum_expression_command.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "sym_differ/sum_expression"
+require "sym_differ/positive_expression"
 
 module SymDiffer
   module ExpressionTextLanguageCompiler
@@ -11,14 +12,23 @@ module SymDiffer
       end
 
       def execute(arguments)
-        expression_a, expression_b = arguments
-        build_sum_expression(expression_a, expression_b)
+        if arguments.size < 2
+          summand = arguments[0]
+          build_positive_expression(summand)
+        else
+          expression_a, expression_b = arguments
+          build_sum_expression(expression_a, expression_b)
+        end
       end
 
       private
 
       def build_sum_expression(expression_a, expression_b)
         @expression_factory.create_sum_expression(expression_a, expression_b)
+      end
+
+      def build_positive_expression(summand)
+        @expression_factory.create_positive_expression(summand)
       end
     end
   end
--- spec/sym_differ/expression_text_language_compiler/build_sum_expression_command_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/build_sum_expression_command_spec.rb
@@ -6,20 +6,34 @@ require "sym_differ/sum_expression"
 
 RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::BuildSumExpressionCommand do
   describe "#execute" do
-    subject(:execute) { described_class.new(expression_factory).execute([expression_a, expression_b]) }
+    subject(:execute) { described_class.new(expression_factory).execute(arguments) }
 
     let(:expression_factory) { build_expression_factory }
 
-    let(:expression_a) { double(:expression_a) }
-    let(:expression_b) { double(:expression_b) }
+    context "when the arguments are expression_a, expression_b" do
+      let(:arguments) { [expression_a, expression_b] }
+      let(:expression_a) { double(:expression_a) }
+      let(:expression_b) { double(:expression_b) }
 
-    it { is_expected.to have_attributes(expression_a:, expression_b:) }
+      it { is_expected.to have_attributes(expression_a:, expression_b:) }
+    end
+
+    context "when the arguments are 1 summand" do
+      let(:arguments) { [summand] }
+      let(:summand) { double(:summand) }
+
+      it { is_expected.to have_attributes(summand:) }
+    end
 
     define_method(:build_expression_factory) do
       expression_factory = double(:expression_factory)
 
       allow(expression_factory).to receive(:create_sum_expression) do |expression_a, expression_b|
         double(:sum_expression, expression_a:, expression_b:)
+      end
+
+      allow(expression_factory).to receive(:create_positive_expression) do |summand|
+        double(:positive_expression, summand:)
       end
 
       expression_factory
