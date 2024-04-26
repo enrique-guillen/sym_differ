d5a8812aa498875cafe280a3a9423a3f4da6b365
Enrique Guillen <1494guillenenrique@gmail.com>
2024-04-26 17:29:44 +0900

    New MultiplicationTokenChecker

---
+++ lib/sym_differ/expression_text_language_compiler/multiplication_token_checker.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require "sym_differ/expression_text_language_compiler/build_multiplicate_expression_command"
+
+module SymDiffer
+  module ExpressionTextLanguageCompiler
+    # Checks the provided token and pushes a BuildMultiplicateExpressionCommand into the commands stack if applicable.
+    class MultiplicationTokenChecker
+      def initialize(expression_factory)
+        @expression_factory = expression_factory
+      end
+
+      def check(token)
+        return build_not_handled_response unless multiplicate_token?(token)
+
+        multiplicate_expression_command = build_command_type_stack_item(
+          build_multiplicate_expression_command
+        )
+
+        build_handled_response(:infix, multiplicate_expression_command)
+      end
+
+      private
+
+      def build_not_handled_response
+        { handled: false }
+      end
+
+      def build_handled_response(expression_location, stack_item)
+        { handled: true, expression_location:, stack_item: }
+      end
+
+      def build_command_type_stack_item(value)
+        { item_type: :pending_command, precedence: 2, value: }
+      end
+
+      def multiplicate_token?(token)
+        token.is_a?(OperatorToken) && token.symbol == "*"
+      end
+
+      def build_multiplicate_expression_command
+        BuildMultiplicateExpressionCommand.new(@expression_factory)
+      end
+    end
+  end
+end
---
+++ spec/sym_differ/expression_text_language_compiler/multiplication_token_checker_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/expression_text_language_compiler/multiplication_token_checker"
+
+require "sym_differ/expression_text_language_compiler/build_multiplicate_expression_command"
+
+RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::MultiplicationTokenChecker do
+  describe "#check" do
+    subject(:check) { described_class.new(expression_factory).check(token) }
+
+    let(:expression_factory) { double(:expression_factory) }
+
+    context "when the provided token is *" do
+      let(:token) { operator_token("*") }
+
+      it "returns an expression and sets expression location as infix" do
+        expect(check).to include(
+          handled: true,
+          expression_location: :infix,
+          stack_item: {
+            item_type: :pending_command,
+            precedence: 2,
+            value: a_kind_of(SymDiffer::ExpressionTextLanguageCompiler::BuildMultiplicateExpressionCommand)
+          }
+        )
+      end
+    end
+
+    context "when the provided token is -" do
+      let(:token) { operator_token("-") }
+
+      it { is_expected.to eq(handled: false) }
+    end
+
+    define_method(:operator_token) do |symbol|
+      SymDiffer::ExpressionTextLanguageCompiler::OperatorToken.new(symbol)
+    end
+  end
+end
