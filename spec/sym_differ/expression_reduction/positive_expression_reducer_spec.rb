89eae68c852bda72a5873a9adee6e9892381d7d8
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-01 21:01:10 +0900

    PositiveExpressionReducer spec utilizes new same_as spec abstractions

--- spec/sym_differ/expression_reduction/positive_expression_reducer_spec.rb
+++ spec/sym_differ/expression_reduction/positive_expression_reducer_spec.rb
@@ -10,12 +10,12 @@ RSpec.describe SymDiffer::ExpressionReduction::PositiveExpressionReducer do
     subject(:reduce) { described_class.new(reducer).reduce(expression) }
 
     before do
-      allow(reducer)
-        .to receive(:reduction_analysis)
-        .with(summand)
-        .and_return(reduced_expression: reduced_summand,
-                    sum_partition: [5, reduced_summand_sum_partition_subexpression],
-                    factor_partition: [2, reduced_summand_factor_partition_subexpression])
+      map_reduction_analysis(
+        from: summand,
+        to: reduction_results(reduced_summand,
+                              sum_partition(5, reduced_summand_sum_partition_subexpression),
+                              factor_partition(2, reduced_summand_factor_partition_subexpression))
+      )
     end
 
     let(:reducer) { double(:reducer) }
@@ -31,10 +31,31 @@ RSpec.describe SymDiffer::ExpressionReduction::PositiveExpressionReducer do
 
     it "returns the reductions of the summand expression" do
       expect(reduce).to include(
-        reduced_expression: reduced_summand,
-        sum_partition: [5, reduced_summand_sum_partition_subexpression],
-        factor_partition: [2, reduced_summand_factor_partition_subexpression]
+        reduction_results(
+          reduced_summand,
+          sum_partition(5, reduced_summand_sum_partition_subexpression),
+          factor_partition(2, reduced_summand_factor_partition_subexpression)
+        )
       )
+    end
+
+    define_method(:map_reduction_analysis) do |from:, to:, input: from, output: to|
+      allow(reducer)
+        .to receive(:reduction_analysis)
+        .with(input)
+        .and_return(output)
+    end
+
+    define_method(:reduction_results) do |reduced_expression, sum_partition, factor_partition = nil|
+      { reduced_expression:, sum_partition:, factor_partition: }.compact
+    end
+
+    define_method(:sum_partition) do |constant, subexpression|
+      [constant, subexpression]
+    end
+
+    define_method(:factor_partition) do |constant, subexpression|
+      [constant, subexpression]
     end
   end
 end
