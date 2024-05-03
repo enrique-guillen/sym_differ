1f28b772a39ea35722929c61def4d930cb3d37c7
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-03 16:36:32 +0900

    Use same_as matcher in build_subtract_expression_command_spec.rb

--- spec/sym_differ/expression_text_language_compiler/commands/build_subtract_expression_command_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/commands/build_subtract_expression_command_spec.rb
@@ -2,43 +2,46 @@
 
 require "spec_helper"
 require "sym_differ/expression_text_language_compiler/commands/build_subtract_expression_command"
+
 require "sym_differ/expressions/subtract_expression"
+require "sym_differ/expression_factory"
 
 RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::Commands::BuildSubtractExpressionCommand do
   describe "#execute" do
     subject(:execute) { described_class.new(expression_factory).execute(arguments) }
 
-    let(:expression_factory) { build_expression_factory }
+    let(:expression_factory) { SymDiffer::ExpressionFactory.new }
 
     context "when the arguments have two expressions" do
+      before do
+        allow(minuend).to receive(:same_as?).with(minuend).and_return(true)
+        allow(subtrahend).to receive(:same_as?).with(subtrahend).and_return(true)
+      end
+
       let(:arguments) { [minuend, subtrahend] }
 
       let(:minuend) { double(:minuend) }
       let(:subtrahend) { double(:subtrahend) }
 
-      it { is_expected.to have_attributes(minuend:, subtrahend:) }
+      it { is_expected.to be_same_as(subtract_expression(minuend, subtrahend)) }
     end
 
     context "when the arguments have only one expression" do
+      before { allow(negated_expression).to receive(:same_as?).with(negated_expression).and_return(true) }
+
       let(:arguments) { [negated_expression] }
 
       let(:negated_expression) { double(:negated_expression) }
 
-      it { is_expected.to have_attributes(negated_expression:) }
+      it { is_expected.to be_same_as(negate_expression(negated_expression)) }
     end
 
-    define_method(:build_expression_factory) do
-      expression_factory = double(:expression_factory)
+    define_method(:subtract_expression) do |minuend, subtrahend|
+      expression_factory.create_subtract_expression(minuend, subtrahend)
+    end
 
-      allow(expression_factory).to receive(:create_subtract_expression) do |minuend, subtrahend|
-        double(:subtract_expression, minuend:, subtrahend:)
-      end
-
-      allow(expression_factory).to receive(:create_negate_expression) do |negated_expression|
-        double(:negate_expression, negated_expression:)
-      end
-
-      expression_factory
+    define_method(:negate_expression) do |negated_expression|
+      expression_factory.create_negate_expression(negated_expression)
     end
   end
 end
