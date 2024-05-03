e0ae64fe48f6be7115fef368543389ecf06e5644
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-03 16:38:54 +0900

    Use same_as matcher in build_sum_expression_command_spec.rb

--- spec/sym_differ/expression_text_language_compiler/commands/build_sum_expression_command_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/commands/build_sum_expression_command_spec.rb
@@ -3,39 +3,44 @@
 require "spec_helper"
 require "sym_differ/expression_text_language_compiler/commands/build_sum_expression_command"
 
+require "sym_differ/expression_factory"
+
 RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::Commands::BuildSumExpressionCommand do
   describe "#execute" do
     subject(:execute) { described_class.new(expression_factory).execute(arguments) }
 
-    let(:expression_factory) { build_expression_factory }
+    let(:expression_factory) { SymDiffer::ExpressionFactory.new }
 
     context "when the arguments are expression_a, expression_b" do
+      before do
+        allow(expression_a).to receive(:same_as?).with(expression_a).and_return(true)
+        allow(expression_b).to receive(:same_as?).with(expression_b).and_return(true)
+      end
+
       let(:arguments) { [expression_a, expression_b] }
       let(:expression_a) { double(:expression_a) }
       let(:expression_b) { double(:expression_b) }
 
-      it { is_expected.to have_attributes(expression_a:, expression_b:) }
+      it { is_expected.to be_same_as(sum_expression(expression_a, expression_b)) }
     end
 
     context "when the arguments are 1 summand" do
+      before do
+        allow(summand).to receive(:same_as?).with(summand).and_return(true)
+      end
+
       let(:arguments) { [summand] }
       let(:summand) { double(:summand) }
 
-      it { is_expected.to have_attributes(summand:) }
+      it { is_expected.to be_same_as(positive_expression(summand)) }
     end
 
-    define_method(:build_expression_factory) do
-      expression_factory = double(:expression_factory)
+    define_method(:sum_expression) do |expression_a, expression_b|
+      expression_factory.create_sum_expression(expression_a, expression_b)
+    end
 
-      allow(expression_factory).to receive(:create_sum_expression) do |expression_a, expression_b|
-        double(:sum_expression, expression_a:, expression_b:)
-      end
-
-      allow(expression_factory).to receive(:create_positive_expression) do |summand|
-        double(:positive_expression, summand:)
-      end
-
-      expression_factory
+    define_method(:positive_expression) do |summand|
+      expression_factory.create_positive_expression(summand)
     end
   end
 end
