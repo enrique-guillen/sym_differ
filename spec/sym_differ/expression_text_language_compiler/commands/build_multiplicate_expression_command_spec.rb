e19d9eb3a4671d6810d94e0f1fe2a3dc371806cf
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-03 16:31:13 +0900

    Use same_as matcher in build_multiplicate_expression_command_spec.rb

--- spec/sym_differ/expression_text_language_compiler/commands/build_multiplicate_expression_command_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/commands/build_multiplicate_expression_command_spec.rb
@@ -9,12 +9,21 @@ RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::Commands::BuildMultipl
   describe "#execute" do
     subject(:execute) { described_class.new(expression_factory).execute(arguments) }
 
+    before do
+      allow(multiplicand).to receive(:same_as?).with(multiplicand).and_return(true)
+      allow(multiplier).to receive(:same_as?).with(multiplier).and_return(true)
+    end
+
     let(:expression_factory) { SymDiffer::ExpressionFactory.new }
     let(:arguments) { [multiplicand, multiplier] }
 
     let(:multiplicand) { double(:multiplicand) }
     let(:multiplier) { double(:multiplier) }
 
-    it { is_expected.to have_attributes(multiplicand:, multiplier:) }
+    it { is_expected.to be_same_as(multiplicate_expression(multiplicand, multiplier)) }
+
+    define_method(:multiplicate_expression) do |multiplicand, multiplier|
+      expression_factory.create_multiplicate_expression(multiplicand, multiplier)
+    end
   end
 end
