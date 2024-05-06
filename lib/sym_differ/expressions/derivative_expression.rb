b8b2883e81d79505826c0da534ce462f9eb44ff3
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-06 20:42:40 +0900

    New DerivativeExpression#same_as?

--- lib/sym_differ/expressions/derivative_expression.rb
+++ lib/sym_differ/expressions/derivative_expression.rb
@@ -2,6 +2,7 @@
 
 module SymDiffer
   module Expressions
+    # Represents a not-yet-evaluated derivative of an expression.
     class DerivativeExpression
       def initialize(underived_expression, variable)
         @underived_expression = underived_expression
@@ -9,6 +10,12 @@ module SymDiffer
       end
 
       attr_reader :underived_expression, :variable
+
+      def same_as?(other_expression)
+        other_expression.is_a?(DerivativeExpression) &&
+          other_expression.underived_expression.same_as?(underived_expression) &&
+          other_expression.variable.same_as?(variable)
+      end
     end
   end
 end
--- spec/sym_differ/expressions/derivative_expression_spec.rb
+++ spec/sym_differ/expressions/derivative_expression_spec.rb
@@ -3,6 +3,8 @@
 require "spec_helper"
 require "sym_differ/expressions/derivative_expression"
 
+require "sym_differ/expressions/constant_expression"
+
 RSpec.describe SymDiffer::Expressions::DerivativeExpression do
   describe "#initialize" do
     subject(:expression) { described_class.new(underived_expression, variable) }
@@ -11,5 +13,44 @@ RSpec.describe SymDiffer::Expressions::DerivativeExpression do
     let(:variable) { double(:variable) }
 
     it { is_expected.to have_attributes(underived_expression:, variable:) }
+  end
+
+  describe "#same_as?" do
+    subject(:same_as?) { expression.same_as?(other_expression) }
+
+    let(:expression_factory) { sym_differ_expression_factory }
+
+    let(:expression) { described_class.new(underived_expression, variable) }
+    let(:underived_expression) { double(:underived_expression) }
+    let(:variable) { double(:variable) }
+
+    context "when other expression = 1" do
+      let(:other_expression) { constant_expression(1) }
+
+      it { is_expected.to be(false) }
+    end
+
+    context "when other expression = derive_expression(x, x)" do
+      before do
+        allow(underived_expression).to receive(:same_as?).with(underived_expression).and_return(true)
+        allow(variable).to receive(:same_as?).with(variable).and_return(true)
+      end
+
+      let(:other_expression) { described_class.new(underived_expression, variable) }
+
+      it { is_expected.to be(true) }
+    end
+
+    context "when other expression = derive_expression(x, y)" do
+      before do
+        allow(underived_expression).to receive(:same_as?).with(underived_expression).and_return(true)
+        allow(y_variable).to receive(:same_as?).with(variable).and_return(false)
+      end
+
+      let(:other_expression) { described_class.new(underived_expression, y_variable) }
+      let(:y_variable) { double(:y_variable) }
+
+      it { is_expected.to be(false) }
+    end
   end
 end
