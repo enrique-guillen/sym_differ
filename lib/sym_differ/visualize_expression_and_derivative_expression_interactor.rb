690ba8dd65b87a1523c372d5f7ab02f24313c2a8
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-13 18:31:55 +0900

    New VisualizeExpressionAndDerivativeExpressionInteractor

---
+++ lib/sym_differ/visualize_expression_and_derivative_expression_interactor.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require "sym_differ/expression_and_derivative_expression_visualizer"
+require "sym_differ/expression_text_language_compiler/parser"
+require "sym_differ/differentiation/differentiation_visitor"
+require "sym_differ/expression_reducer"
+require "sym_differ/stringifier_visitor"
+require "sym_differ/differentiation_graph/expression_path_generator"
+require "sym_differ/expression_evaluator_visitor"
+require "sym_differ/differentiation_graph/svg_graph_view_renderer"
+require "sym_differ/differentiation_graph/step_range"
+
+module SymDiffer
+  # Implements the use case for a user getting the graph image of an expression and its derivative.
+  class VisualizeExpressionAndDerivativeExpressionInteractor
+    def initialize(view_renderer = SymDiffer::DifferentiationGraph::SvgGraphViewRenderer.new)
+      @view_renderer = view_renderer
+    end
+
+    def visualize(expression_text, variable)
+      visualizer(variable).visualize(expression_text, variable)
+    end
+
+    private
+
+    def visualizer(variable)
+      ExpressionAndDerivativeExpressionVisualizer.new(
+        expression_parser,
+        differentiation_visitor(variable),
+        expression_reducer,
+        expression_stringifier,
+        expression_path_generator,
+        @view_renderer,
+        build_step_range(-10..10)
+      )
+    end
+
+    def expression_parser
+      ExpressionTextLanguageCompiler::Parser.new(expression_factory)
+    end
+
+    def differentiation_visitor(variable)
+      Differentiation::DifferentiationVisitor.new(variable, expression_factory)
+    end
+
+    def expression_reducer
+      ExpressionReducer.new(expression_factory)
+    end
+
+    def expression_stringifier
+      StringifierVisitor.new
+    end
+
+    def expression_path_generator
+      SymDiffer::DifferentiationGraph::ExpressionPathGenerator.new(0.125, expression_evaluator_builder)
+    end
+
+    def build_step_range(range)
+      SymDiffer::DifferentiationGraph::StepRange.new(range)
+    end
+
+    def expression_evaluator_builder
+      ExpressionEvaluatorBuilder.new
+    end
+
+    def expression_factory
+      @expression_factory = ExpressionFactory.new
+    end
+
+    # Builds an instance of an ExpressionEvaluator that will evaluate the expression with the variables set at the
+    # specified values.
+    class ExpressionEvaluatorBuilder
+      def build(variable_values)
+        ExpressionEvaluatorVisitor.new(variable_values)
+      end
+    end
+  end
+end
---
+++ spec/sym_differ/visualize_expression_and_derivative_expression_interactor_spec.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/visualize_expression_and_derivative_expression_interactor"
+
+require "sym_differ/differentiation_graph/evaluation_point"
+
+RSpec.describe SymDiffer::VisualizeExpressionAndDerivativeExpressionInteractor do
+  describe "#visualize" do
+    subject(:visualize) do
+      described_class.new(view_renderer).visualize("2 * x", "x")
+    end
+
+    before { allow(view_renderer).to receive(:render).and_return(rendered_view) }
+
+    let(:view_renderer) { double(:view_renderer) }
+    let(:rendered_view) { double(:rendered_view) }
+
+    let(:expected_expression_path) do
+      [
+        evaluation_point(-50, -50.0), evaluation_point(-40.0, -40.0), evaluation_point(-30.0, -30.0),
+        evaluation_point(-20.0, -20.0), evaluation_point(-10.0, -10.0), evaluation_point(0.0, 0.0),
+        evaluation_point(10.0, 10.0), evaluation_point(20.0, 20.0), evaluation_point(30.0, 30.0),
+        evaluation_point(40.0, 40.0), evaluation_point(50.0, 50.0)
+      ]
+    end
+
+    let(:expected_derivative_expression_path) do
+      [
+        evaluation_point(-50, 5.0), evaluation_point(-40.0, 5.0), evaluation_point(-30.0, 5.0),
+        evaluation_point(-20.0, 5.0), evaluation_point(-10.0, 5.0), evaluation_point(0.0, 5.0),
+        evaluation_point(10.0, 5.0), evaluation_point(20.0, 5.0), evaluation_point(30.0, 5.0),
+        evaluation_point(40.0, 5.0), evaluation_point(50.0, 5.0)
+      ]
+    end
+
+    it { is_expected.to eq(rendered_view) }
+
+    it "renders the expected view" do
+      visualize
+
+      expect(view_renderer).to have_received(:render).with(an_object_having_attributes(show_total_area_aid: false))
+    end
+
+    it "renders the expected abscissa axis" do
+      visualize
+
+      expect(view_renderer).to have_received(:render).with(
+        an_object_having_attributes(
+          abscissa_axis: an_object_having_attributes(
+            name: "x", origin: 50, offset: 0.0,
+            number_labels: [-10.0, -8.0, -6.0, -4.0, -2.0, 0.0, 2.0, 4.0, 6.0, 8.0, 10.0]
+          )
+        )
+      )
+    end
+
+    it "renders the expected ordinate axis" do
+      visualize
+
+      expect(view_renderer).to have_received(:render).with(
+        an_object_having_attributes(
+          ordinate_axis: an_object_having_attributes(
+            name: "y", origin: 50, offset: 0.0,
+            number_labels: [-20.0, -16.0, -12.0, -8.0, -4.0, 0.0, 4.0, 8.0, 12.0, 16.0, 20.0]
+          )
+        )
+      )
+    end
+
+    it "renders the expected expression graph" do
+      visualize
+
+      expect(view_renderer).to have_received(:render).with(
+        an_object_having_attributes(
+          expression_graph: an_object_having_attributes(
+            text: "2 * x",
+            path: a_collection_including(*expected_expression_path.map(&method(:same_evaluation_point_as)))
+          )
+        )
+      )
+    end
+
+    it "renders the expected derivative expression graph" do
+      visualize
+
+      expect(view_renderer).to have_received(:render).with(
+        an_object_having_attributes(
+          derivative_expression_graph: an_object_having_attributes(
+            text: "2",
+            path: a_collection_including(*expected_derivative_expression_path.map(&method(:same_evaluation_point_as)))
+          )
+        )
+      )
+    end
+
+    define_method(:evaluation_point) do |abscissa, ordinate|
+      SymDiffer::DifferentiationGraph::EvaluationPoint.new(abscissa, ordinate)
+    end
+  end
+end
