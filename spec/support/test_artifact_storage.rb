d6bbc39d24c2c1412d8b70eed60b5269d155cc42
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-14 21:33:14 +0900

    RSpec component test support for automatically storing hashes of test artifacts after storing images

--- spec/support/test_artifact_storage.rb
+++ spec/support/test_artifact_storage.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
+require "digest"
+require "digest/bubblebabble"
+
 module Support
   # Support for storing post-test artifacts that form part of the test suite's output but not printed as part of the
   # inmediate report (e.g., rspec logs).
   module TestArtifactStorage
     def write_test_artifact_path(file_name, contents)
-      path = test_artifact_path(file_name)
+      file_path = test_artifact_path(file_name)
+      hash_path = test_artifact_hash_path("#{file_name}.svg")
 
-      File.write(path, contents)
+      File.write(file_path, contents)
+      File.write(hash_path, ::Digest::SHA256.bubblebabble(contents))
     end
 
     def filesystem_friendlify_class_name(class_name)
@@ -18,6 +23,11 @@ module Support
 
     def test_artifact_path(file_name)
       directory = %w[spec test_artifacts].join("/")
+      [directory, file_name].join("/")
+    end
+
+    def test_artifact_hash_path(file_name)
+      directory = %w[spec test_artifact_hashes].join("/")
       [directory, file_name].join("/")
     end
   end
