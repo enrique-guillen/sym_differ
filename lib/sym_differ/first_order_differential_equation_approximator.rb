d1c9ca124343b4c5493df5429b375de8c1ad6f60
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-15 21:19:30 +0900

    New FirstOrderDifferentialEquationApproximator

---
+++ lib/sym_differ/first_order_differential_equation_approximator.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module SymDiffer
+  # Implements the use case for a user getting the approximation of the solutiuon to a first-order differential
+  # equation.
+  class FirstOrderDifferentialEquationApproximator
+    def initialize(expression_text_parser, solution_approximator)
+      @expression_text_parser = expression_text_parser
+      @solution_approximator = solution_approximator
+    end
+
+    def approximate_solution(expression_text, y_variable_name, t_variable_name, initial_value_coordinates)
+      y_variable_name = "y" if y_variable_name.empty?
+      t_variable_name = "t" if t_variable_name.empty?
+
+      @y_variable_name = y_variable_name
+      @t_variable_name = t_variable_name
+      @initial_value_coordinates = initial_value_coordinates
+
+      validate_variables_and_approximate_solution(expression_text)
+    end
+
+    private
+
+    attr_reader :y_variable_name, :t_variable_name, :initial_value_coordinates
+
+    def validate_variables_and_approximate_solution(expression_text)
+      validate_variable(y_variable_name)
+      validate_variable(t_variable_name)
+
+      parse_expression_text(expression_text)
+        .then { |expression| approximate_solution_via_approximator(expression) }
+    end
+
+    def validate_variable(variable_name)
+      @expression_text_parser.validate_variable(variable_name)
+    end
+
+    def parse_expression_text(text)
+      @expression_text_parser.parse(text)
+    end
+
+    def approximate_solution_via_approximator(expression)
+      @solution_approximator
+        .approximate_solution(expression, y_variable_name, t_variable_name, initial_value_coordinates)
+    end
+  end
+end
---
+++ spec/sym_differ/first_order_differential_equation_approximator_spec.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/first_order_differential_equation_approximator"
+
+require "sym_differ/error"
+
+RSpec.describe SymDiffer::FirstOrderDifferentialEquationApproximator do
+  describe "#approximate_solution" do
+    subject(:approximate_solution) do
+      described_class
+        .new(expression_text_parser, solution_approximator)
+        .approximate_solution(expression_text, y_variable_name, t_variable_name, initial_value_coordinates)
+    end
+
+    before do
+      allow(expression_text_parser)
+        .to receive(:parse)
+        .with("y")
+        .and_return(variable_expression("y"))
+
+      allow(expression_text_parser)
+        .to receive(:parse)
+        .with("")
+        .and_raise(SymDiffer::Error, "Invalid expression")
+
+      allow(expression_text_parser).to receive(:validate_variable).with("y").and_return(true)
+      allow(expression_text_parser).to receive(:validate_variable).with("x").and_return(true)
+      allow(expression_text_parser).to receive(:validate_variable).with("t").and_return(true)
+
+      allow(expression_text_parser)
+        .to receive(:validate_variable)
+        .with("123invalid")
+        .and_raise(SymDiffer::Error, "Invalid variable")
+    end
+
+    let(:expression_factory) { sym_differ_expression_factory }
+
+    let(:expression_text_parser) { double(:expression_text_parser) }
+    let(:solution_approximator) { double(:solution_approximator) }
+
+    context "when expression = y, y-var name = y, t-var name = x, initial value coordinates = (0, 1)" do
+      before do
+        allow(solution_approximator)
+          .to receive(:approximate_solution)
+          .with(same_expression_as(variable_expression("y")), "y", "x", coordinates(0, 1))
+          .and_return(solution_approximation)
+      end
+
+      let(:expression_text) { "y" }
+      let(:y_variable_name) { "y" }
+      let(:t_variable_name) { "x" }
+      let(:initial_value_coordinates) { coordinates(0, 1) }
+
+      let(:solution_approximation) { double(:solution_approximation) }
+
+      it { is_expected.to eq(solution_approximation) }
+    end
+
+    context "when expression = , y-var name = y, t-var name = x, initial value coordinates = (0, 1)" do
+      let(:expression_text) { "" }
+      let(:y_variable_name) { "y" }
+      let(:t_variable_name) { "x" }
+      let(:initial_value_coordinates) { coordinates(0, 1) }
+
+      let(:solution_approximation) { double(:solution_approximation) }
+
+      it "lets SymDiffer::Error be raised" do
+        expect { approximate_solution }.to raise_error(SymDiffer::Error, "Invalid expression")
+      end
+    end
+
+    context "when expression = y, y-var name = 123invalid, t-var name = x, initial value coordinates = (0, 1)" do
+      let(:expression_text) { "y" }
+      let(:y_variable_name) { "123invalid" }
+      let(:t_variable_name) { "x" }
+      let(:initial_value_coordinates) { coordinates(0, 1) }
+
+      let(:solution_approximation) { double(:solution_approximation) }
+
+      it "lets SymDiffer::Error be raised" do
+        expect { approximate_solution }.to raise_error(SymDiffer::Error, "Invalid variable")
+      end
+    end
+
+    context "when expression = y, y-var name = y, t-var name = 123invalid, initial value coordinates = (0, 1)" do
+      let(:expression_text) { "y" }
+      let(:y_variable_name) { "y" }
+      let(:t_variable_name) { "123invalid" }
+      let(:initial_value_coordinates) { coordinates(0, 1) }
+
+      let(:solution_approximation) { double(:solution_approximation) }
+
+      it "lets SymDiffer::Error be raised" do
+        expect { approximate_solution }.to raise_error(SymDiffer::Error, "Invalid variable")
+      end
+    end
+
+    context "when expression = y, y-var name = <empty>, t-var name = <empty>, initial value coordinates = (0, 1)" do
+      before do
+        allow(solution_approximator)
+          .to receive(:approximate_solution)
+          .with(same_expression_as(variable_expression("y")), "y", "t", coordinates(0, 1))
+          .and_return(solution_approximation)
+      end
+
+      let(:expression_text) { "y" }
+      let(:y_variable_name) { "" }
+      let(:t_variable_name) { "" }
+      let(:initial_value_coordinates) { coordinates(0, 1) }
+
+      let(:solution_approximation) { double(:solution_approximation) }
+
+      it { is_expected.to eq(solution_approximation) }
+    end
+
+    define_method(:coordinates) do |abscissa, ordinate|
+      [abscissa, ordinate]
+    end
+  end
+end
