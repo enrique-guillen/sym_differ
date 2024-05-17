063bb9d005687430d37d695e1038307f64af94d1
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-17 17:06:08 +0900

    New GetFirstOrderDifferentialEquationApproximationInteractor

---
+++ lib/sym_differ/get_first_order_differential_equation_approximation_interactor.rb
@@ -0,0 +1,70 @@
+# frozen_string_literal: true
+
+require "sym_differ/first_order_differential_equation_approximator"
+require "sym_differ/expression_text_language_compiler/parser"
+require "sym_differ/expression_factory"
+require "sym_differ/runge_kutta_four_solution_approximator"
+
+module SymDiffer
+  # Implements the use case for a user getting the approximation of the solutiuon to a first-order differential
+  # equation.
+  class GetFirstOrderDifferentialEquationApproximationInteractor
+    # Defines the high-level response of this use case.
+    OperationResponse = Struct.new(:approximated_solution)
+
+    def approximate_solution(expression_text, y_variable_name, t_variable_name, initial_value_coordinates)
+      starting_abscissa = access_asbcissa_of_coordinates(initial_value_coordinates)
+
+      step_range_starting_point = (starting_abscissa + 0.125)
+      step_range_last_point = (starting_abscissa + 10.0)
+      step_range = build_step_range(step_range_starting_point..step_range_last_point)
+
+      approximated_solution =
+        first_order_differential_equation_approximator
+        .approximate_solution(expression_text, y_variable_name, t_variable_name, initial_value_coordinates, step_range)
+
+      build_operation_response(approximated_solution)
+    end
+
+    private
+
+    def access_asbcissa_of_coordinates(coordinates)
+      coordinates[0]
+    end
+
+    def build_step_range(range)
+      StepRange.new(range)
+    end
+
+    def first_order_differential_equation_approximator
+      FirstOrderDifferentialEquationApproximator.new(expression_parser, runge_kutta_four_solution_approximator)
+    end
+
+    def build_operation_response(*attributes)
+      OperationResponse.new(*attributes)
+    end
+
+    def expression_parser
+      ExpressionTextLanguageCompiler::Parser.new(expression_factory)
+    end
+
+    def runge_kutta_four_solution_approximator
+      RungeKuttaFourSolutionApproximator.new(expression_evaluator_adapter, 0.125)
+    end
+
+    def expression_factory
+      ExpressionFactory.new
+    end
+
+    def expression_evaluator_adapter
+      ExpressionEvaluatorAdapter.new
+    end
+
+    # Adapts the ExpressionEvaluatorVisitor interface to the one expected by the first ODE solution approximator.
+    class ExpressionEvaluatorAdapter
+      def evaluate(expression, variable_values)
+        ExpressionEvaluatorVisitor.new(variable_values).evaluate(expression)
+      end
+    end
+  end
+end
---
+++ spec/sym_differ/get_first_order_differential_equation_approximation_interactor_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/get_first_order_differential_equation_approximation_interactor"
+
+require "sym_differ/evaluation_point"
+
+RSpec.describe SymDiffer::GetFirstOrderDifferentialEquationApproximationInteractor do
+  describe "#approximate_solution" do
+    subject(:approximate_solution) do
+      described_class.new.approximate_solution(
+        expression_text, y_variable_name, t_variable_name, initial_value_coordinates
+      )
+    end
+
+    let(:expression_text) { "y" }
+    let(:y_variable_name) { "y" }
+    let(:t_variable_name) { "x" }
+
+    context "when initial coordinates = 0.0, 1.0" do
+      let(:initial_value_coordinates) { coordinates(0.0, 1.0) }
+
+      it "returns the expected approximate solution" do
+        expect(approximate_solution).to have_attributes(
+          approximated_solution: a_collection_including(
+            same_evaluation_point_as(evaluation_point(0.0, 1.0)),
+            same_evaluation_point_as(evaluation_point(0.125, 1.1271974540051117)),
+            same_evaluation_point_as(evaluation_point(1.0, 2.6061535098540802)),
+            same_evaluation_point_as(evaluation_point(5.0, 120.22643420198703)),
+            same_evaluation_point_as(evaluation_point(10.0, 14_454.395480924717))
+          )
+        )
+      end
+    end
+
+    context "when initial coordinates = 1.0, <Math::E>" do
+      let(:initial_value_coordinates) { coordinates(1.0, Math::E) }
+
+      it "returns the expected approximate solution" do
+        expect(approximate_solution).to have_attributes(
+          approximated_solution: a_collection_including(
+            same_evaluation_point_as(evaluation_point(1.0, Math::E)),
+            same_evaluation_point_as(evaluation_point(1.125, 3.0640403563073955)),
+            same_evaluation_point_as(evaluation_point(6.0, 326.80933139168866)),
+            same_evaluation_point_as(evaluation_point(11.0, 39_291.120577158246))
+          )
+        )
+      end
+    end
+
+    define_method(:coordinates) do |abscissa, ordinate|
+      [abscissa, ordinate]
+    end
+
+    define_method(:evaluation_point) do |abscissa, ordinate|
+      SymDiffer::EvaluationPoint.new(abscissa, ordinate)
+    end
+  end
+end
