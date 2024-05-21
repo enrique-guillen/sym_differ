5641e35173de3bb92db0154dd08d7984b9897dc6
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-21 21:50:25 +0900

    New FirstOrderDifferentialEquationApproximation

---
+++ lib/sym_differ/first_order_differential_equation_approximation_illustration/graph_parameters_calculator.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module SymDiffer
+  module FirstOrderDifferentialEquationApproximationIllustration
+    # Outputs information about the expression graphs that must be honored regardless of view size.
+    class GraphParametersCalculator
+      def calculate(approximation_expression_path)
+        abscissa_parameters = extract_abscissas_parameters(approximation_expression_path)
+        ordinate_parameters = extract_ordinate_parameters(approximation_expression_path)
+
+        abscissa_parameters.merge(ordinate_parameters)
+      end
+
+      private
+
+      def extract_ordinate_parameters(approximation_expression_path)
+        ordinates = extract_ordinates(approximation_expression_path)
+        max_ordinate_value = ordinates.max
+        min_ordinate_value = ordinates.min
+        ordinate_distance = max_ordinate_value - min_ordinate_value
+
+        { max_ordinate_value:, min_ordinate_value:, ordinate_distance: }
+      end
+
+      def extract_abscissas_parameters(approximation_expression_path)
+        abscissas = extract_abscissas(approximation_expression_path)
+        max_abscissa_value = abscissas.max
+        min_abscissa_value = abscissas.min
+        abscissa_distance = max_abscissa_value - min_abscissa_value
+
+        { max_abscissa_value:, min_abscissa_value:, abscissa_distance: }
+      end
+
+      def extract_ordinates(expression_path)
+        expression_path.map(&:ordinate)
+      end
+
+      def extract_abscissas(expression_path)
+        expression_path.map(&:abscissa)
+      end
+    end
+  end
+end
---
+++ spec/sym_differ/first_order_differential_equation_approximation_illustration/graph_parameters_calculator_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/first_order_differential_equation_approximation_illustration/graph_parameters_calculator"
+
+require "sym_differ/first_order_differential_equation_solution/equation_parameters"
+
+RSpec.describe SymDiffer::FirstOrderDifferentialEquationApproximationIllustration::GraphParametersCalculator do
+  describe "#calculate" do
+    subject(:calculate) do
+      described_class.new.calculate(approximation_expression_path)
+    end
+
+    let(:numerical_analysis_item_factory) { sym_differ_numerical_analysis_item_factory }
+
+    let(:approximation_expression_path) do
+      [
+        create_evaluation_point(0.0, 1.0), create_evaluation_point(1.0, 2.6)
+      ]
+    end
+
+    it "returns the expected parameters" do
+      expect(calculate).to include(
+        max_ordinate_value: 2.6, min_ordinate_value: 1.0, ordinate_distance: 1.6,
+        max_abscissa_value: 1.0, min_abscissa_value: 0.0, abscissa_distance: 1.0
+      )
+    end
+  end
+end
