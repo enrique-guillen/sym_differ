889e18747989506ffefba3af566f5095e6b91bce
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-26 20:36:06 +0900

    New NewtonMethodRootFinder

---
+++ lib/sym_differ/newton_method_root_finder.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require "sym_differ/fixed_point_approximator"
+
+module SymDiffer
+  # Finds a variable value for which the given expression yields a value close to 0.0, by implementing the Newton
+  # Method's steps.
+  class NewtonMethodRootFinder
+    def initialize(
+      derivative_slope_width,
+      tolerance,
+      expression_evaluator,
+      fixed_point_finder_creator = FixedPointFinderCreator.new
+    )
+      @derivative_slope_width = derivative_slope_width
+      @tolerance = tolerance
+      @expression_evaluator = expression_evaluator
+      @fixed_point_finder_creator = fixed_point_finder_creator
+    end
+
+    def find(expression, variable, first_guess)
+      expression_newton_transform_evaluator =
+        create_expression_newton_transform_evaluator(variable)
+
+      fixed_point_finder =
+        create_fixed_point_finder(expression_newton_transform_evaluator)
+
+      fixed_point_finder.approximate(expression, variable, first_guess)
+    end
+
+    private
+
+    def create_expression_newton_transform_evaluator(variable)
+      ExpressionNewtonTransformEvaluator
+        .new(@derivative_slope_width, variable, @expression_evaluator)
+    end
+
+    def create_fixed_point_finder(expression_evaluator)
+      @fixed_point_finder_creator.create(@tolerance, expression_evaluator)
+    end
+
+    # Wraps the provided ExpressionEvaluator into an evaluator that returns a NewtonTransformed-method whose fixed
+    # points approximate the roots of the provided expressions.
+    class ExpressionNewtonTransformEvaluator
+      def initialize(derivative_slope_width, variable_name, expression_evaluator)
+        @derivative_slope_width = derivative_slope_width
+        @variable_name = variable_name
+        @expression_evaluator = expression_evaluator
+      end
+
+      def evaluate(expression, variable_values)
+        variable_value = variable_values.fetch(@variable_name)
+
+        expression_at_variable_value = evaluate_expression(expression, variable_values)
+        derivative_at_variable_value = numerically_approximate_derivative(expression, variable_value, variable_values)
+
+        expression_to_derivative_ratio = expression_at_variable_value / derivative_at_variable_value
+
+        variable_value - expression_to_derivative_ratio
+      end
+
+      def numerically_approximate_derivative(expression, variable_value, variable_values)
+        parameter_1 = merge_variable(variable_values, variable_value + @derivative_slope_width)
+        parameter_2 = merge_variable(variable_values, variable_value)
+
+        value_1 = evaluate_expression(expression, parameter_1)
+        value_2 = evaluate_expression(expression, parameter_2)
+
+        slope_height = value_1 - value_2
+
+        slope_height / @derivative_slope_width
+      end
+
+      private
+
+      def merge_variable(variable_values, value)
+        variable_values.merge(@variable_name => value)
+      end
+
+      def evaluate_expression(expression, variable_values)
+        @expression_evaluator.evaluate(expression, variable_values)
+      end
+    end
+  end
+end
---
+++ spec/sym_differ/newton_method_root_finder_spec.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/newton_method_root_finder"
+
+RSpec.describe SymDiffer::NewtonMethodRootFinder do
+  describe "#find" do
+    subject(:find) do
+      described_class
+        .new(derivative_approximation_dx,
+             tolerance,
+             expression_evaluator,
+             fixed_point_finder_creator)
+        .find(expression, variable, first_guess)
+    end
+
+    before do
+      allow(expression_evaluator)
+        .to receive(:evaluate)
+        .with(expression, a_hash_including("x" => anything)) do |_expression, variable_values|
+          variable_values.fetch("x")
+        end
+
+      expected_evaluator_matcher =
+        an_object_satisfying do |evaluator|
+          evaluator.evaluate(expression, "x" => 1.0) + 6.07747097092215e-09 == 0.0
+        end
+
+      allow(fixed_point_finder_creator)
+        .to receive(:create)
+        .with(tolerance,
+              expected_evaluator_matcher)
+        .and_return(fixed_point_finder)
+
+      allow(fixed_point_finder)
+        .to receive(:approximate)
+        .with(expression, variable, first_guess)
+        .and_return(fixed_point_finder_result)
+    end
+
+    let(:derivative_approximation_dx) { 0.00000001 }
+    let(:tolerance) { 0.00000001 }
+    let(:expression_evaluator) { double(:expression_evaluator) }
+
+    let(:fixed_point_finder_creator) do
+      double(:fixed_point_finder_creator)
+    end
+
+    let(:fixed_point_finder) do
+      double(:fixed_point_finder)
+    end
+
+    let(:expression) { double(:expression) }
+    let(:variable) { "x" }
+    let(:first_guess) { 1.0 }
+
+    let(:fixed_point_finder_result) do
+      double(:fixed_point_finder_result)
+    end
+
+    it "calls the fixed-point finder with the newton-transform of the expression and more" do
+      expect(find).to eq(fixed_point_finder_result)
+    end
+  end
+end
