b0504308fb06a9b9d68ea58f0f32dbbdfca49a09
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-27 18:04:39 +0900

    Use Forwardable for expression_factory methods in NegateExpressionDeriver

--- lib/sym_differ/differentiation/negate_expression_deriver.rb
+++ lib/sym_differ/differentiation/negate_expression_deriver.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
+require "forwardable"
+
 module SymDiffer
   module Differentiation
     # Computes the derivative of the provided expression in the form -subexpression.
     class NegateExpressionDeriver
+      extend Forwardable
+
       def initialize(differentiation_visitor, expression_factory)
         @differentiation_visitor = differentiation_visitor
         @expression_factory = expression_factory
       end
 
       def derive(expression)
-        build_negate_expression(derive_expression(expression))
+        create_negate_expression(derive_expression(expression))
       end
 
       private
 
-      def build_negate_expression(negated_expression)
-        @expression_factory.create_negate_expression(negated_expression)
-      end
-
       def derive_expression(expression)
         expression.negated_expression.accept(@differentiation_visitor)
       end
+
+      def_delegators :@expression_factory, :create_negate_expression
     end
   end
 end
