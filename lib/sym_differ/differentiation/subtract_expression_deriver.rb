d5d7dd07c00b062d6620b74811439bccf3f439bc
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-27 18:07:30 +0900

    Use Forwardable for expression_factory methods in SubtractExpressionDeriver

--- lib/sym_differ/differentiation/subtract_expression_deriver.rb
+++ lib/sym_differ/differentiation/subtract_expression_deriver.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
 
-require "sym_differ/expressions/subtract_expression"
+require "forwardable"
 
 module SymDiffer
   module Differentiation
     # Computes the expression that represents the derivative of a subtraction expression.
     class SubtractExpressionDeriver
+      extend Forwardable
+
       def initialize(differentiation_visitor, expression_factory)
         @differentiation_visitor = differentiation_visitor
         @expression_factory = expression_factory
       end
 
       def derive(expression)
-        build_subtract_expression(
+        create_subtract_expression(
           derive_expression(expression.minuend),
           derive_expression(expression.subtrahend)
         )
@@ -20,13 +22,11 @@ module SymDiffer
 
       private
 
-      def build_subtract_expression(minuend, subtrahend)
-        @expression_factory.create_subtract_expression(minuend, subtrahend)
-      end
-
       def derive_expression(expression)
         expression.accept(@differentiation_visitor)
       end
+
+      def_delegators :@expression_factory, :create_subtract_expression
     end
   end
 end
