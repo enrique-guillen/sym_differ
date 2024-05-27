e0f69acd04eda4a56ee0a12d5fd471c664c995dd
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-27 18:44:54 +0900

    Address MethodLength rubocop issue in DivideExpressionDeriver

--- lib/sym_differ/differentiation/divide_expression_deriver.rb
+++ lib/sym_differ/differentiation/divide_expression_deriver.rb
@@ -14,18 +14,35 @@ module SymDiffer
       end
 
       def derive(expression)
-        create_divide_expression(
-          create_subtract_expression(
-            create_multiplicate_expression(derive_expression(expression.numerator), expression.denominator),
-            create_multiplicate_expression(expression.numerator, derive_expression(expression.denominator))
-          ),
-          create_exponentiate_expression(
-            expression.denominator, create_constant_expression(2)
-          )
-        )
+        numerator = create_numerator_of_derivative(expression)
+        denominator = create_denominator_of_derivative(expression)
+
+        create_divide_expression(numerator, denominator)
       end
 
       private
+
+      def create_numerator_of_derivative(expression)
+        create_subtract_expression(
+          create_minuend_of_derivative(expression),
+          create_subtrahend_of_derivative(expression)
+        )
+      end
+
+      def create_denominator_of_derivative(expression)
+        create_exponentiate_expression(
+          expression.denominator,
+          create_constant_expression(2)
+        )
+      end
+
+      def create_minuend_of_derivative(expression)
+        create_multiplicate_expression(derive_expression(expression.numerator), expression.denominator)
+      end
+
+      def create_subtrahend_of_derivative(expression)
+        create_multiplicate_expression(expression.numerator, derive_expression(expression.denominator))
+      end
 
       def derive_expression(expression)
         expression.accept(@deriver)
