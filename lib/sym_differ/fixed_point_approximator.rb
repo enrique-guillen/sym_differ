b6c5aec66fb02e09a43df92507dac219ac11c9ca
Enrique Guillen <1494guillenenrique@gmail.com>
2024-05-30 22:10:38 +0900

    In FixedPointApproximator, damp averages and limit how many times the fixed point expression can be evaluated

--- lib/sym_differ/fixed_point_approximator.rb
+++ lib/sym_differ/fixed_point_approximator.rb
@@ -3,8 +3,9 @@
 module SymDiffer
   # Returns a value that's close or equal to a fixed point of a function (points where f(x) = x).
   class FixedPointApproximator
-    def initialize(tolerance, expression_evaluator)
+    def initialize(tolerance, max_evaluations, expression_evaluator)
       @tolerance = tolerance
+      @max_evaluations = max_evaluations
       @expression_evaluator = expression_evaluator
     end
 
@@ -12,7 +13,9 @@ module SymDiffer
       @expression = expression
       @variable = variable
 
-      new_guess = evaluate_expression(first_guess)
+      reset_current_evaluation_amount
+
+      new_guess = evaluate_average_damp(first_guess)
       distance = calculate_distance_between_guesses(first_guess, new_guess)
       evaluate_expression_until_output_distance_low(first_guess, new_guess, distance)
     end
@@ -21,7 +24,10 @@ module SymDiffer
 
     def evaluate_expression_until_output_distance_low(current_guess, new_guess, distance_between_guesses)
       while greater_than_tolerance?(distance_between_guesses)
-        new_guess = evaluate_expression(current_guess)
+        return if current_evaluation_amount_exceeds_max?
+
+        bump_current_evaluation_amount
+        new_guess = evaluate_average_damp(current_guess)
 
         distance_between_guesses = calculate_distance_between_guesses(current_guess, new_guess)
         current_guess = new_guess
@@ -38,10 +44,29 @@ module SymDiffer
       distance > @tolerance
     end
 
+    def evaluate_average_damp(value)
+      next_value = evaluate_expression(value)
+
+      (value + next_value) / 2
+    end
+
+    def reset_current_evaluation_amount
+      @current_evaluation_amount = 0
+    end
+
+    def bump_current_evaluation_amount
+      self.current_evaluation_amount += 1
+    end
+
+    def current_evaluation_amount_exceeds_max?
+      self.current_evaluation_amount >= @max_evaluations
+    end
+
     def evaluate_expression(value)
       @expression_evaluator.evaluate(expression, variable => value)
     end
 
+    attr_accessor :current_evaluation_amount
     attr_reader :expression, :variable
   end
 end
--- spec/sym_differ/fixed_point_approximator_spec.rb
+++ spec/sym_differ/fixed_point_approximator_spec.rb
@@ -7,10 +7,11 @@ RSpec.describe SymDiffer::FixedPointApproximator do
   describe "#approximate" do
     subject(:approximate) do
       described_class
-        .new(tolerance, expression_evaluator)
+        .new(tolerance, max_evaluations, expression_evaluator)
         .approximate(expression, variable, first_guess)
     end
 
+    let(:max_evaluations) { 1_000 }
     let(:expression_evaluator) { double(:expression_evaluator) }
 
     let(:expression_factory) { sym_differ_expression_factory }
@@ -37,7 +38,7 @@ RSpec.describe SymDiffer::FixedPointApproximator do
       context "when first_guess = 9" do
         let(:first_guess) { 9.0 }
 
-        it { is_expected.to eq(1.0) }
+        it { is_expected.to eq(5.0) }
       end
     end
 
@@ -56,13 +57,13 @@ RSpec.describe SymDiffer::FixedPointApproximator do
       context "when first_guess = 1.0" do
         let(:first_guess) { 1.0 }
 
-        it { is_expected.to eq(0.5) }
+        it { is_expected.to eq(0.75) }
       end
 
       context "when first_guess = 30.0" do
         let(:first_guess) { 30.0 }
 
-        it { is_expected.to eq(7.5) }
+        it { is_expected.to eq(22.5) }
       end
     end
 
@@ -81,8 +82,59 @@ RSpec.describe SymDiffer::FixedPointApproximator do
       context "when first_guess = -30.0" do
         let(:first_guess) { -30.0 }
 
-        it { is_expected.to eq(0.9375) }
+        it { is_expected.to eq(-0.46875) }
       end
+    end
+
+    context "when the expression is 1 / x, tolerance = .00001, first guess 2.0" do
+      before do
+        allow(expression_evaluator)
+          .to receive(:evaluate)
+          .with(expression, a_hash_including("x" => anything)) do |_expression, variable_values|
+            1.0 / variable_values.fetch(variable)
+          end
+      end
+
+      let(:expression) { double(:expression) }
+      let(:tolerance) { 0.00001 }
+
+      let(:first_guess) { 2.0 }
+
+      it { is_expected.to eq(1.000000000000001) }
+    end
+
+    context "when the expression is 2 / x, tolerance = .00001, first guess 2.0 (clarification)" do
+      before do
+        allow(expression_evaluator)
+          .to receive(:evaluate)
+          .with(expression, a_hash_including("x" => anything)) do |_expression, variable_values|
+            2.0 / variable_values.fetch(variable)
+          end
+      end
+
+      let(:expression) { double(:expression) }
+      let(:tolerance) { 0.00001 }
+
+      let(:first_guess) { 2.0 }
+
+      it { is_expected.to eq(1.4142135623746899) }
+    end
+
+    context "when the expression is x + 1, tolerance = .00001, first guess 2.0" do
+      before do
+        allow(expression_evaluator)
+          .to receive(:evaluate)
+          .with(expression, a_hash_including("x" => anything)) do |_expression, variable_values|
+            variable_values.fetch(variable) + 1.0
+          end
+      end
+
+      let(:expression) { double(:expression) }
+      let(:tolerance) { 0.00001 }
+
+      let(:first_guess) { 2.0 }
+
+      it { is_expected.to be_nil }
     end
   end
 end
