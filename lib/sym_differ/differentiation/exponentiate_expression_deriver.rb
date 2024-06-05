278460ad4ef3dd950a4c3fc291e7f1662794b126
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-05 12:08:57 +0900

    New ExponentiateExpressionDeriver

---
+++ lib/sym_differ/differentiation/exponentiate_expression_deriver.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require "forwardable"
+
+module SymDiffer
+  module Differentiation
+    # Computes the expression that represents the derivative of a base expression raised to another power expression.
+    class ExponentiateExpressionDeriver
+      extend Forwardable
+
+      def initialize(differentiation_visitor, expression_walker, expression_factory)
+        @differentiation_visitor = differentiation_visitor
+        @expression_walker = expression_walker
+        @expression_factory = expression_factory
+      end
+
+      def derive(expression, variable)
+        if expression_contains_no_variables?(expression.power, variable)
+          return derivative_of_expression_raised_to_constant_power(expression)
+        end
+
+        if expression.base.same_as?(create_euler_number_expression)
+          return derivative_of_euler_number_raised_to_expression_power(expression)
+        end
+
+        derivative_of_arbitrary_exponentiation(expression)
+      end
+
+      private
+
+      def expression_contains_no_variables?(expression, variable)
+        was_no_variable_detected = true
+
+        walk_variable_expressions(expression) do |subexpression|
+          (break was_no_variable_detected = false) if subexpression.name == variable
+        end
+
+        was_no_variable_detected
+      end
+
+      def derivative_of_expression_raised_to_constant_power(expression)
+        base = expression.base
+        power = expression.power
+
+        create_multiplicate_expression(
+          create_multiplicate_expression(
+            power,
+            create_exponentiate_expression(base, create_subtract_expression(power, create_constant_expression(1)))
+          ),
+          derive_expression(base)
+        )
+      end
+
+      def derivative_of_euler_number_raised_to_expression_power(expression)
+        create_multiplicate_expression(
+          expression,
+          derive_expression(expression.power)
+        )
+      end
+
+      def derivative_of_arbitrary_exponentiation(expression)
+        derive_expression(
+          create_natural_logarithm_expression(
+            create_exponentiate_expression(
+              create_euler_number_expression, expression
+            )
+          )
+        )
+      end
+
+      def derive_expression(expression)
+        @differentiation_visitor.derive(expression)
+      end
+
+      def walk_variable_expressions(expression, &)
+        walk_expression(expression, yield_at: %i[variables], &)
+      end
+
+      def_delegators :@expression_factory,
+                     :create_multiplicate_expression,
+                     :create_exponentiate_expression,
+                     :create_subtract_expression,
+                     :create_constant_expression,
+                     :create_euler_number_expression,
+                     :create_natural_logarithm_expression
+
+      def_delegator :@expression_walker, :walk, :walk_expression
+    end
+  end
+end
---
+++ spec/sym_differ/differentiation/exponentiate_expression_deriver_spec.rb
@@ -0,0 +1,112 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/differentiation/exponentiate_expression_deriver"
+
+RSpec.describe SymDiffer::Differentiation::ExponentiateExpressionDeriver do
+  describe "#derive" do
+    subject(:derive) do
+      described_class
+        .new(differentiation_visitor, expression_walker, expression_factory)
+        .derive(expression, variable)
+    end
+
+    let(:differentiation_visitor) { double(:differentiation_visitor) }
+    let(:expression_walker) { double(:expression_walker) }
+    let(:expression_factory) { sym_differ_expression_factory }
+
+    let(:expression) { exponentiate_expression(base, power) }
+    let(:variable) { "x" }
+
+    context "when the power is a constant" do
+      before do
+        allow(expression_walker)
+          .to receive(:walk)
+          .with(power, yield_at: %i[variables])
+
+        allow(differentiation_visitor)
+          .to receive(:derive)
+          .with(base)
+          .and_return(base_derivative)
+      end
+
+      let(:base) { expression_test_double(:base) }
+      let(:power) { constant_expression(5) }
+      let(:base_derivative) { expression_test_double(:base_derivative) }
+
+      it "returns the derivative of a function raised to a constant" do
+        expect(derive).to be_same_as(
+          multiplicate_expression(
+            multiplicate_expression(
+              constant_expression(5),
+              exponentiate_expression(base, subtract_expression(constant_expression(5), constant_expression(1)))
+            ),
+            base_derivative
+          )
+        )
+      end
+    end
+
+    context "when the base is the euler number expression" do
+      before do
+        allow(expression_walker)
+          .to receive(:walk)
+          .with(power, yield_at: %i[variables])
+          .and_yield(variable_expression("x"))
+
+        allow(differentiation_visitor)
+          .to receive(:derive)
+          .with(power)
+          .and_return(power_derivative)
+      end
+
+      let(:base) { euler_number_expression }
+      let(:power) { expression_test_double(:power) }
+      let(:power_derivative) { expression_test_double(:power_derivative) }
+
+      it "returns the derivative of euler raised to an expression" do
+        expect(derive).to be_same_as(
+          multiplicate_expression(
+            exponentiate_expression(euler_number_expression, power),
+            power_derivative
+          )
+        )
+      end
+    end
+
+    context "when both base and power are arbitrary expressions" do
+      before do
+        allow(expression_walker)
+          .to receive(:walk)
+          .with(power, yield_at: %i[variables])
+          .and_yield(variable_expression("x"))
+
+        allow(differentiation_visitor)
+          .to receive(:derive)
+          .with(
+            same_expression_as(
+              natural_logarithm_expression(
+                exponentiate_expression(
+                  euler_number_expression, exponentiate_expression(base, power)
+                )
+              )
+            )
+          )
+          .and_return(arbitrary_exponentiation_expression_derivative)
+      end
+
+      let(:base) { expression_test_double(:expression) }
+      let(:power) { expression_test_double(:power) }
+
+      let(:arbitrary_exponentiation_expression_derivative) do
+        expression_test_double(:arbitrary_exponentiation_expression_derivative)
+      end
+
+      it "returns the derivative of the natural logarithm of the euler number raised to the given expression" do
+        expect(derive).to be_same_as(
+          arbitrary_exponentiation_expression_derivative
+        )
+      end
+    end
+  end
+end
