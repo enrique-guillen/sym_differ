45a4483804b1572d469cc8f87a03a0cf4897728c
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-07 11:01:45 +0900

    New MultiplicateExpressionReductionAnalyzer

---
+++ lib/sym_differ/expression_reduction/multiplicate_expression_reduction_analyzer.rb
@@ -0,0 +1,115 @@
+# frozen_string_literal: true
+
+module SymDiffer
+  module ExpressionReduction
+    # Generates an analysis of how the multiplicate expression can re-expressed for reduction purposes, after removing
+    # the superfluous terms and putting it into canonical form.
+    class MultiplicateExpressionReductionAnalyzer
+      def initialize(expression_factory, sum_partitioner, factor_partitioner)
+        @expression_factory = expression_factory
+        @sum_partitioner = sum_partitioner
+        @factor_partitioner = factor_partitioner
+      end
+
+      def reduce_expression(expression)
+        reduction_analysis = generate_reduction_analysis(expression)
+
+        total_factor, multiplicand_subexpression, multiplier_subexpression =
+          reduction_analysis.values_at(:total_factor, :multiplicand_subexpression, :multiplier_subexpression)
+
+        create_reduced_expression(total_factor, multiplicand_subexpression, multiplier_subexpression)
+      end
+
+      def make_factor_partition(expression)
+        reduction_analysis = generate_reduction_analysis(expression)
+
+        total_factor, multiplicand_subexpression, multiplier_subexpression =
+          reduction_analysis.values_at(:total_factor, :multiplicand_subexpression, :multiplier_subexpression)
+
+        create_factor_partition(total_factor, multiplicand_subexpression, multiplier_subexpression)
+      end
+
+      def make_sum_partition(expression)
+        reduced_expression = reduce_expression(expression)
+        create_sum_partition(reduced_expression)
+      end
+
+      private
+
+      def generate_reduction_analysis(expression)
+        multiplicand_reduction_results = factor_partition(expression.multiplicand)
+        multiplier_reduction_results = factor_partition(expression.multiplier)
+
+        multiplicand_factor, multiplicand_subexpression = multiplicand_reduction_results
+        multiplier_factor, multiplier_subexpression = multiplier_reduction_results
+
+        total_factor = multiplicand_factor * multiplier_factor
+
+        { total_factor:, multiplicand_subexpression:, multiplier_subexpression: }
+      end
+
+      def create_reduced_expression(total_factor, multiplicand_subexpression, multiplier_subexpression)
+        return create_constant_expression(0) if total_factor.zero?
+
+        nested_expression =
+          create_reduced_expression_from_subexpressions(multiplicand_subexpression, multiplier_subexpression)
+
+        return create_constant_expression(total_factor) if nested_expression.nil?
+        return nested_expression if total_factor == 1
+
+        create_multiplicate_expression(create_constant_expression(total_factor), nested_expression)
+      end
+
+      def create_sum_partition(expression)
+        return build_sum_partition(0, expression) if expression.is_a?(Expressions::MultiplicateExpression)
+
+        sum_partition(expression)
+      end
+
+      def create_factor_partition(total_factor, multiplicand_subexpression, multiplier_subexpression)
+        nested_expression =
+          if total_factor.negative? || total_factor.positive?
+            create_reduced_expression_from_subexpressions(multiplicand_subexpression, multiplier_subexpression)
+          end
+
+        build_factor_partition(total_factor, nested_expression)
+      end
+
+      def create_reduced_expression_from_subexpressions(multiplicand_subexpression, multiplier_subexpression)
+        return if multiplicand_subexpression.nil? && multiplier_subexpression.nil?
+        return multiplicand_subexpression if multiplier_subexpression.nil?
+        return multiplier_subexpression if multiplicand_subexpression.nil?
+
+        create_multiplicate_expression(multiplicand_subexpression, multiplier_subexpression)
+      end
+
+      def factor_partition(expression)
+        @factor_partitioner.partition(expression)
+      end
+
+      def sum_partition(expression)
+        @sum_partitioner.partition(expression)
+      end
+
+      def create_constant_expression(value)
+        @expression_factory.create_constant_expression(value)
+      end
+
+      def create_multiplicate_expression(multiplicand, multiplier)
+        @expression_factory.create_multiplicate_expression(multiplicand, multiplier)
+      end
+
+      def build_reduction_results(reduced_expression, sum_partition, factor_partition)
+        { reduced_expression:, sum_partition:, factor_partition: }
+      end
+
+      def build_sum_partition(constant, subexpression)
+        [constant, subexpression]
+      end
+
+      def build_factor_partition(constant, subexpression)
+        [constant, subexpression]
+      end
+    end
+  end
+end
---
+++ spec/sym_differ/expression_reduction/multiplicate_expression_reduction_analyzer_spec.rb
@@ -0,0 +1,294 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/expression_reduction/multiplicate_expression_reduction_analyzer"
+
+RSpec.describe SymDiffer::ExpressionReduction::MultiplicateExpressionReductionAnalyzer do
+  let(:reduction_analyzer) do
+    described_class.new(
+      expression_factory,
+      sum_partitioner,
+      factor_partitioner
+    )
+  end
+
+  let(:expression_factory) { sym_differ_expression_factory }
+  let(:sum_partitioner) { double(:sum_partitioner) }
+  let(:factor_partitioner) { double(:factor_partitioner) }
+
+  describe "#reduce_expression" do
+    subject(:reduce_expression) do
+      reduction_analyzer.reduce_expression(expression)
+    end
+
+    context "when the expression is x*x" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(1, variable_expression("x")))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(1, variable_expression("x")))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results x * x, [0, x * x]" do
+        expected_reduced_expression = multiplicate_expression(variable_expression("x"), variable_expression("x"))
+
+        expect(reduce_expression).to be_same_as(expected_reduced_expression)
+      end
+    end
+
+    context "when the expression is 2*2" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(2, nil))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(2, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results 4, [4, nil], [4, nil]" do
+        expect(reduce_expression).to be_same_as(constant_expression(4))
+      end
+    end
+
+    context "when the expression is x * 1" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(1, variable_expression("x")))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(1, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results x, [0, x]" do
+        expect(reduce_expression).to be_same_as(variable_expression("x"))
+      end
+    end
+
+    context "when the expression is 1 * x" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(1, nil))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(1, variable_expression("x")))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results x, [0, x]" do
+        expect(reduce_expression).to be_same_as(variable_expression("x"))
+      end
+    end
+
+    context "when the expression is x * 0" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(1, variable_expression("x")))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(0, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results 0, [0, 0]" do
+        expect(reduce_expression).to be_same_as(constant_expression(0))
+      end
+    end
+
+    context "when the expression is 3x * 3" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(3, variable_expression("x")))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(3, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results 9x, [0, 9x], [9, x]" do
+        expect(reduce_expression).to be_same_as(
+          multiplicate_expression(constant_expression(9), variable_expression("x"))
+        )
+      end
+    end
+
+    define_method(:map_reduction_analysis) do |from:, to:, input: from, output: to|
+      allow(expression)
+        .to receive(:accept)
+        .with(input)
+        .and_return(output)
+    end
+  end
+
+  describe "#make_factor_partition" do
+    subject(:make_factor_partition) do
+      reduction_analyzer.make_factor_partition(expression)
+    end
+
+    context "when the expression is 3x * 3" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(3, variable_expression("x")))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(3, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results 9x, [0, 9x], [9, x]" do
+        expected_factor_partition_subexpression = variable_expression("x")
+
+        expect(make_factor_partition).to match(
+          factor_partition(9, same_expression_as(expected_factor_partition_subexpression))
+        )
+      end
+    end
+  end
+
+  describe "#make_sum_partition" do
+    subject(:make_sum_partition) do
+      reduction_analyzer.make_sum_partition(expression)
+    end
+
+    context "when the expression is 3x * 3" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(3, variable_expression("x")))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(3, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the reduction results 9x, [0, 9x], [9, x]" do
+        expected_sum_partition_subexpression =
+          multiplicate_expression(constant_expression(9), variable_expression("x"))
+
+        expect(make_sum_partition).to match(
+          sum_partition(0, same_expression_as(expected_sum_partition_subexpression))
+        )
+      end
+    end
+
+    context "when the expression is 2*2" do
+      before do
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplicand)
+          .and_return(factor_partition(2, nil))
+
+        allow(factor_partitioner)
+          .to receive(:partition)
+          .with(multiplier)
+          .and_return(factor_partition(2, nil))
+
+        allow(sum_partitioner)
+          .to receive(:partition)
+          .with(same_expression_as(constant_expression(4)))
+          .and_return(sum_partition(4, nil))
+      end
+
+      let(:expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:multiplicand) { double(:multiplicand) }
+      let(:multiplier) { double(:multiplier) }
+
+      it "returns the expected sum partition" do
+        expect(make_sum_partition).to match(sum_partition(4, nil))
+      end
+    end
+  end
+
+  define_method(:factor_partition) do |constant, subexpression|
+    [constant, subexpression]
+  end
+
+  define_method(:sum_partition) do |constant, subexpression|
+    [constant, subexpression]
+  end
+end
