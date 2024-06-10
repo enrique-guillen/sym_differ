a816a69e360589b869e9a5ee8a82a09986c9ee8c
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-11 04:32:20 +0900

    New ImbalancedExpressionError#accept

--- lib/sym_differ/expression_text_language_compiler/imbalanced_expression_error.rb
+++ lib/sym_differ/expression_text_language_compiler/imbalanced_expression_error.rb
@@ -6,6 +6,10 @@ module SymDiffer
   module ExpressionTextLanguageCompiler
     # Raised when the expression text was terminated without the precedence levels being balanced by the time the
     # expression was fully read.
-    class ImbalancedExpressionError < SymDiffer::Error; end
+    class ImbalancedExpressionError < SymDiffer::Error
+      def accept(visitor)
+        visitor.visit_imbalanced_expression_error(self)
+      end
+    end
   end
 end
--- spec/sym_differ/expression_text_language_compiler/imbalanced_expression_error_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/imbalanced_expression_error_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::ImbalancedExpressionEr
 
     it { is_expected }
   end
+
+  describe "#accept" do
+    subject(:accept) { error.accept(visitor) }
+
+    before do
+      allow(visitor)
+        .to receive(:visit_imbalanced_expression_error)
+        .with(error)
+        .and_return(visit_result)
+    end
+
+    let(:error) { described_class.new }
+    let(:visitor) { double(:visitor) }
+    let(:visit_result) { double(:visit_result) }
+
+    it { is_expected.to eq(visit_result) }
+  end
 end
