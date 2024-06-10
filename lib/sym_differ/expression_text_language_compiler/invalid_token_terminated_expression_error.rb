b60d51a2ff2ec7a67d6592bb128ee61bc8f0db3f
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-11 04:27:07 +0900

    New InvalidTokenTerminatedExpressionError

--- lib/sym_differ/expression_text_language_compiler/invalid_token_terminated_expression_error.rb
+++ lib/sym_differ/expression_text_language_compiler/invalid_token_terminated_expression_error.rb
@@ -6,6 +6,10 @@ module SymDiffer
   module ExpressionTextLanguageCompiler
     # Raised when the expression text to compile was terminated in a gramatically-incorrect token, thus making it not
     # possible to convert the expression text into a single expression.
-    class InvalidTokenTerminatedExpressionError < SymDiffer::Error; end
+    class InvalidTokenTerminatedExpressionError < SymDiffer::Error
+      def accept(visitor)
+        visitor.visit_invalid_token_terminated_expression_error(self)
+      end
+    end
   end
 end
--- spec/sym_differ/expression_text_language_compiler/invalid_token_terminated_expression_error_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/invalid_token_terminated_expression_error_spec.rb
@@ -5,8 +5,25 @@ require "sym_differ/expression_text_language_compiler/invalid_token_terminated_e
 
 RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::InvalidTokenTerminatedExpressionError do
   describe ".new" do
-    subject(:new) { described_class.new }
+    subject(:error) { described_class.new }
 
     it { is_expected }
+  end
+
+  describe "#accept" do
+    subject(:accept) { error.accept(visitor) }
+
+    before do
+      allow(visitor)
+        .to receive(:visit_invalid_token_terminated_expression_error)
+        .with(error)
+        .and_return(visit_result)
+    end
+
+    let(:error) { described_class.new }
+    let(:visitor) { double(:visitor) }
+    let(:visit_result) { double(:visit_result) }
+
+    it { is_expected.to eq(visit_result) }
   end
 end
