d8b054712aa5ec6c81fafadc45c4e2dfee329e1f
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-11 04:42:08 +0900

    New UnrecognizedFunctionNameError#accept

--- lib/sym_differ/expression_text_language_compiler/unrecognized_function_name_error.rb
+++ lib/sym_differ/expression_text_language_compiler/unrecognized_function_name_error.rb
@@ -7,6 +7,9 @@ module SymDiffer
     # In the expression text, a part of the expression was correct function-application syntax, but the name of the
     # function is not recognized.
     class UnrecognizedFunctionNameError < SymDiffer::Error
+      def accept(visitor)
+        visitor.visit_unrecognized_function_name_error(self)
+      end
     end
   end
 end
--- spec/sym_differ/expression_text_language_compiler/unrecognized_function_name_error_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/unrecognized_function_name_error_spec.rb
@@ -11,4 +11,21 @@ RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::UnrecognizedFunctionNa
       expect { new }.not_to raise_error
     end
   end
+
+  describe "#accept" do
+    subject(:accept) { error.accept(visitor) }
+
+    before do
+      allow(visitor)
+        .to receive(:visit_unrecognized_function_name_error)
+        .with(error)
+        .and_return(visit_result)
+    end
+
+    let(:error) { described_class.new }
+    let(:visitor) { double(:visitor) }
+    let(:visit_result) { double(:visit_result) }
+
+    it { is_expected.to eq(visit_result) }
+  end
 end
