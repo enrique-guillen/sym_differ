886d0e8ae76662dd9918581ce15cf0ec1016d57d
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-11 04:28:35 +0900

    New ExpectedTokentypeNotFoundError#accept

--- lib/sym_differ/expression_text_language_compiler/expected_token_type_not_found_error.rb
+++ lib/sym_differ/expression_text_language_compiler/expected_token_type_not_found_error.rb
@@ -6,6 +6,10 @@ module SymDiffer
   module ExpressionTextLanguageCompiler
     # Raised when the expression text tokens were being read and, at some point, a token of the expected token types
     # was not found.
-    class ExpectedTokenTypeNotFoundError < SymDiffer::Error; end
+    class ExpectedTokenTypeNotFoundError < SymDiffer::Error
+      def accept(visitor)
+        visitor.visit_expected_token_type_not_found_error(self)
+      end
+    end
   end
 end
