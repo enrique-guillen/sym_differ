9dddccaae839332537137f5c57c2da7abf93193c
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-10 17:07:03 +0900

    New NaturalLogarithmExpressionReductionAnalyzer

---
+++ lib/sym_differ/expression_reduction/natural_logarithm_expression_reduction_analyzer.rb
@@ -0,0 +1,82 @@
+# frozen_string_literal: true
+
+require "forwardable"
+
+module SymDiffer
+  module ExpressionReduction
+    # Generates an analysis of how the natural-logarithm expression can re-expressed for reduction purposes, after
+    # removing the superfluous terms and putting it into canonical form.
+    class NaturalLogarithmExpressionReductionAnalyzer
+      extend Forwardable
+
+      def initialize(expression_factory, expression_reducer)
+        @expression_factory = expression_factory
+        @expression_reducer = expression_reducer
+      end
+
+      def reduce_expression(expression)
+        reduced_power_expression = reduce_subexpression(expression.power)
+
+        return create_constant_zero if same_as_constant_one?(reduced_power_expression)
+        return create_constant_one if same_as_euler_number_expression?(reduced_power_expression)
+
+        if multiplicate_expression?(reduced_power_expression)
+          return reduced_logarithms_sum_of_multiplication(reduced_power_expression)
+        end
+
+        if exponentiate_expression?(reduced_power_expression)
+          return reduced_scaled_logarithm_of_exponentiation(reduced_power_expression)
+        end
+
+        create_natural_logarithm_expression(reduced_power_expression)
+      end
+
+      private
+
+      def same_as_constant_one?(expression)
+        expression.same_as?(create_constant_one)
+      end
+
+      def same_as_euler_number_expression?(expression)
+        expression.same_as?(create_euler_number_expression)
+      end
+
+      def create_constant_one
+        create_constant_expression(1)
+      end
+
+      def create_constant_zero
+        create_constant_expression(0)
+      end
+
+      def reduced_logarithms_sum_of_multiplication(expression)
+        reduce_subexpression(
+          create_sum_expression(
+            create_natural_logarithm_expression(expression.multiplicand),
+            create_natural_logarithm_expression(expression.multiplier)
+          )
+        )
+      end
+
+      def reduced_scaled_logarithm_of_exponentiation(expression)
+        reduce_subexpression(
+          create_multiplicate_expression(
+            expression.power,
+            create_natural_logarithm_expression(expression.base)
+          )
+        )
+      end
+
+      def_delegator :@expression_reducer, :reduce, :reduce_subexpression
+
+      def_delegators :@expression_factory,
+                     :create_constant_expression,
+                     :create_sum_expression,
+                     :create_multiplicate_expression,
+                     :create_euler_number_expression,
+                     :create_natural_logarithm_expression,
+                     :multiplicate_expression?,
+                     :exponentiate_expression?
+    end
+  end
+end
---
+++ spec/sym_differ/expression_reduction/natural_logarithm_expression_reduction_analyzer_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "sym_differ/expression_reduction/natural_logarithm_expression_reduction_analyzer"
+
+RSpec.describe SymDiffer::ExpressionReduction::NaturalLogarithmExpressionReductionAnalyzer do
+  let(:reduction_analyzer) { described_class.new(expression_factory, expression_reducer) }
+
+  let(:expression_factory) { sym_differ_expression_factory }
+  let(:expression_reducer) { double(:expression_reducer) }
+
+  describe "#reduce_expression" do
+    subject(:reduce_expression) do
+      reduction_analyzer.reduce_expression(expression)
+    end
+
+    before do
+      allow(expression_reducer)
+        .to receive(:reduce)
+        .with(power_expression)
+        .and_return(reduced_power_expression)
+    end
+
+    let(:expression) { natural_logarithm_expression(power_expression) }
+    let(:power_expression) { double(:power_expression) }
+
+    context "when reduced_power_expression is an expression that cannot be reduced" do
+      let(:reduced_power_expression) { expression_test_double(:reduced_power_expression) }
+
+      it { is_expected.to be_same_as(natural_logarithm_expression(reduced_power_expression)) }
+    end
+
+    context "when the reduced power expression is the same as the euler_number_expression" do
+      let(:reduced_power_expression) { euler_number_expression }
+
+      it { is_expected.to be_same_as(constant_expression(1)) }
+    end
+
+    context "when the reduced power expression is the same as 1" do
+      let(:reduced_power_expression) { constant_expression(1) }
+
+      it { is_expected.to be_same_as(constant_expression(0)) }
+    end
+
+    context "when the reduced power expression is the same as x * y" do
+      before do
+        allow(expression_reducer)
+          .to receive(:reduce)
+          .with(
+            same_expression_as(
+              sum_expression(
+                natural_logarithm_expression(multiplicand),
+                natural_logarithm_expression(multiplier)
+              )
+            )
+          )
+          .and_return(reduced_sum_of_logarithms)
+      end
+
+      let(:reduced_power_expression) do
+        multiplicate_expression(multiplicand, multiplier)
+      end
+
+      let(:reduced_sum_of_logarithms) { double(:reduced_sum_of_logarithms) }
+
+      let(:multiplicand) { expression_test_double(:multiplicand) }
+      let(:multiplier) { expression_test_double(:multiplier) }
+
+      it "returns ln(x) + ln(y)" do
+        expect(reduce_expression).to eq(reduced_sum_of_logarithms)
+      end
+    end
+
+    context "when the reduced power expression is the same as x ^ y" do
+      before do
+        allow(expression_reducer)
+          .to receive(:reduce)
+          .with(
+            same_expression_as(
+              multiplicate_expression(
+                power,
+                natural_logarithm_expression(base)
+              )
+            )
+          )
+          .and_return(reduced_multiplication_of_logarithms)
+      end
+
+      let(:reduced_power_expression) do
+        exponentiate_expression(base, power)
+      end
+
+      let(:reduced_multiplication_of_logarithms) do
+        double(:reduced_multiplication_of_logarithms)
+      end
+
+      let(:base) { expression_test_double(:base) }
+      let(:power) { expression_test_double(:power) }
+
+      it "returns y*ln(x))" do
+        expect(reduce_expression).to eq(reduced_multiplication_of_logarithms)
+      end
+    end
+  end
+end
