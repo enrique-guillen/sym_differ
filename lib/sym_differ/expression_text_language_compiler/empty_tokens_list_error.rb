8eaa25b4467d316c93f349da1b83257648f07ea7
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-11 04:37:57 +0900

    New EmptyTokensListError#accept

--- lib/sym_differ/expression_text_language_compiler/empty_tokens_list_error.rb
+++ lib/sym_differ/expression_text_language_compiler/empty_tokens_list_error.rb
@@ -5,6 +5,10 @@ require "sym_differ/error"
 module SymDiffer
   module ExpressionTextLanguageCompiler
     # Raised when the expression text to compile was not tokenized into a non-empty list of tokens.
-    class EmptyTokensListError < SymDiffer::Error; end
+    class EmptyTokensListError < SymDiffer::Error
+      def accept(visitor)
+        visitor.visit_empty_tokens_list_error(self)
+      end
+    end
   end
 end
--- spec/sym_differ/expression_text_language_compiler/empty_tokens_list_error_spec.rb
+++ spec/sym_differ/expression_text_language_compiler/empty_tokens_list_error_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe SymDiffer::ExpressionTextLanguageCompiler::EmptyTokensListError d
 
     it { is_expected }
   end
+
+  describe "#accept" do
+    subject(:accept) { error.accept(visitor) }
+
+    before do
+      allow(visitor)
+        .to receive(:visit_empty_tokens_list_error)
+        .with(error)
+        .and_return(visit_result)
+    end
+
+    let(:error) { described_class.new }
+    let(:visitor) { double(:visitor) }
+    let(:visit_result) { double(:visit_result) }
+
+    it { is_expected.to eq(visit_result) }
+  end
 end
