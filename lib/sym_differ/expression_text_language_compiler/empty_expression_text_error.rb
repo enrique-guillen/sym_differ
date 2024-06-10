f0342d7f30db0863d4822dd3d6ad53eb0f5da3a1
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-11 04:41:25 +0900

    New EmptyExpressionTextError#accept

--- lib/sym_differ/expression_text_language_compiler/empty_expression_text_error.rb
+++ lib/sym_differ/expression_text_language_compiler/empty_expression_text_error.rb
@@ -6,6 +6,9 @@ module SymDiffer
   module ExpressionTextLanguageCompiler
     # Error to raise when an attempt to parse an empty string is made.
     class EmptyExpressionTextError < SymDiffer::Error
+      def accept(visitor)
+        visitor.visit_empty_expression_text_error(self)
+      end
     end
   end
 end
