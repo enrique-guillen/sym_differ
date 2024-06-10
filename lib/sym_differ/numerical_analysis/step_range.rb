2808d4a2cb7e1bc7a8ca9c315e6db6d52a58a32c
Enrique Guillen <1494guillenenrique@gmail.com>
2024-06-10 17:06:15 +0900

    Rewrite StepRange to use Forwardable to define its public methods

--- lib/sym_differ/numerical_analysis/step_range.rb
+++ lib/sym_differ/numerical_analysis/step_range.rb
@@ -1,29 +1,22 @@
 # frozen_string_literal: true
 
+require "forwardable"
+
 module SymDiffer
   module NumericalAnalysis
     # Represents a range of "steps" -- coordinates where the value of a function was sampled. Can be used to specify
     # the range over which a function should be sampled/evaluated.
     class StepRange
+      extend Forwardable
+
       def initialize(range = (0..0))
         @range = range
       end
 
-      def first_element
-        @range.first
-      end
-
-      def last_element
-        @range.last
-      end
-
-      def minimum
-        @range.min
-      end
-
-      def maximum
-        @range.max
-      end
+      def_delegator :@range, :first, :first_element
+      def_delegator :@range, :last, :last_element
+      def_delegator :@range, :min, :minimum
+      def_delegator :@range, :max, :maximum
     end
   end
 end
